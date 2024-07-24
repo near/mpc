2ed7e2cb8c46f494e6488319ddadfcfed4843cae
Xiangyi Zheng <xiangyi@near.org>
2024-07-24 14:34:26 -0700

    use semvar instead

--- chain-signatures/node/src/protocol/mod.rs
+++ chain-signatures/node/src/protocol/mod.rs
@@ -350,9 +350,6 @@ async fn get_my_participant(protocol: &MpcSignProtocol) -> Participant {
 }
 
 fn node_version() -> i64 {
-    let parts: Vec<&str> = env!("CARGO_PKG_VERSION").split('-').collect();
-    parts[0]
-        .split('.')
-        .map(|s| s.parse::<i64>().unwrap())
-        .fold(0, |acc, x| acc * 1000 + x)
+    let version = semver::Version::parse(env!("CARGO_PKG_VERSION")).unwrap();
+    (version.patch + version.minor * 1000 + version.major * 1000000) as i64
 }
