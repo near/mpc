23734aa6a7d448d38dddc16cd77087a1fbcece25
Peter <petru.papucciu@nearone.org>
2024-12-17 04:49:44 +0000

    Updated scripts

--- devnet/deploy.sh
+++ devnet/deploy.sh
@@ -1,69 +1,52 @@
 #!/bin/bash
-set -exo pipefail
+# set -exo pipefail
 
 ##########################################################################################
 # Variable definition block
 ##########################################################################################
 
-# SOURCE="nearone-robin.testnet"
+# Default values 
+REDEPLOY=0
+THRESHOLD=2
+PARTICIPANTS=2
 SUFFIX=$(uuidgen)
-
-REDEPLOY=$1
-THRESHOLD=$2
-PARTICIPANTS=$3
-ZONE=$4
-
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-
+SIGNER="signer-$SUFFIX.testnet"
 
 ##########################################################################################
-# Temp debug block - TODO DELETE IT 
+# Function to display help
 ##########################################################################################
 
-# locals {
-#   old_mpc_nodes = [
-#     {
-#         account              = "mpc-test0-test-key-generation0011111111111.testnet"
-#         cipher_pk            = "9b6a39f87edd287a54430a2a49ba17cc707a75ee85085fe00800a18a1794127b"
-#         account_sk = "ed25519:3ZVYRDC785uX9NgzUt1tWqwimaKxK36ge9NVrEVpy75Mkqt2kx12efeduAfLw8Dx3nWPj82X9mzGeTxLbNXysEAW"
-#         cipher_sk  = "8425790a303989b72fbceee2dd81c5c054d81c21d27a850fda837b1504b045d1"
-#         sign_sk    = "ed25519:668vJprWzofjZRUJfsn58jyov8fXVWMLaacott6raWThLPt7SyzUWmEVyDz6tusPCZRWgLzjzhwMcsjuzJXuLSyE"
-#     },
-#     {
-#         account              = "mpc-test1-test-key-generation0011111111111.testnet"
-#         cipher_pk            = "a3fbc5dec8e2fc3e3be7a0796778940c1e5dcfa0ffa3995caab72d6269278e2b"
-#         account_sk = "ed25519:N9HsLP7XLcep6GzU7UsA4eWscogkcefTnChxer19Z5DQAAT8PopovNGeXvZQpKWqbGhJwGPnKkXDtNX3G4p8td3"
-#         cipher_sk  = "4b7d2c904ee76763044ffdd328348bbabeaf6e82752a3bffc15481668f38ccca"
-#         sign_sk    = "ed25519:4Wwrcp3Qb45bR8EabqMpGx5DS9jWdwQ5UZQJanToXdkZV6QdEj1mK6ntd9vUPVP4EuejfztFSvUGZdJSjXnpDrTL"
-#     },
-#   ]
-#   upgraded_mpc_nodes = [
-#     {
-#         "p2p_public_key": "7651c69c0826a3bb4091186066d489f48f8099106e18e736e0c156c8a2c0c344",
-#     },
-#     {
-#         "p2p_public_key": "2aa60bf15e7f584e7e19b1c160ef13b06255f655bb173d2e5935e3658c28da36",
-#     }
-#   ]
-#   mpc_contract = "signer-test-key-generation0011111111111.testnet"
-# }
+show_help() {
+    echo "Usage: $0 [options]"
+    echo "Options:"
+    echo "  -h, --help           Display this help message"
+    echo "  -r, --redeploy       Specify if redeploy of signer is required. O for false and 1 for true. (default: $REDEPLOY)"
+    echo "  -t, --threshold      Specify a threshold for signing contract. (default: $THRESHOLD)"
+    echo "  -p, --participants   Number of participants to generate. (default: $PARTICIPANTS)"
+}
+##########################################################################################
+# Variable parsing block
+##########################################################################################
 
-# Current output
-# { 
-#     "mpc-test0-0af63531-5195-437c-b158-3a257e2f7caa.testnet": { 
-#     "account_id": "mpc-test0-0af63531-5195-437c-b158-3a257e2f7caa.testnet", 
-#     "cipher_pk": [155,106,57,248,126,221,40,122,84,67,10,42,73,186,23,204,112,122,117,238,133,8,95,224,8,0,161,138,23,148,18,123], 
-#     "sign_pk": "ed25519:GeqmQKHHZVzYTXTysee31YjQx4MEBFoZRtZda2SBLSRE", 
-#     "url": "http://old-mpc-node-0.service.mpc.consul:3000" 
-#     } , 
-#     "mpc-test1-0af63531-5195-437c-b158-3a257e2f7caa.testnet": {
-#      "account_id": "mpc-test1-0af63531-5195-437c-b158-3a257e2f7caa.testnet", 
-#      "cipher_pk": [163,251,197,222,200,226,252,62,59,231,160,121,103,120,148,12,30,93,207,160,255,163,153,92,170,183,45,98,105,39,142,43], 
-#      "sign_pk": "ed25519:HLVhoaoYKw9GXVDtckqhnUhd4UR5qmqGh1zDmeq2tMrS", 
-#      "url": "http://old-mpc-node-1.service.mpc.consul:3000" 
-#      }      
-# }
-
+while [ ! -z "$1" ]; do
+   if [[ "$1" == "--help" ]] || [[ "$1" == "-h" ]]; then
+      show_help; exit 0;
+   elif [[ "$1" == "-r" ]] || [[ "$1" == "--redeploy" ]]; then
+      REDEPLOY="$2"
+      shift
+   elif [[ $1 == "-t" ]] || [[ "$1" == "--threshold" ]]; then
+      THRESHOLD="$2"
+      shift
+   elif [[ $1 == "-p" ]] || [[ "$1" == "--participants" ]]; then
+      PARTICIPANTS="$2"
+      shift
+   else
+      echo "Incorrect input provided $1"
+      show_help; exit 0;
+   fi
+shift
+done
 
 ##########################################################################################
 # Functions block
@@ -110,7 +93,7 @@ generate_key() {
         \"sign_secret_key\": \"$SIGN_SECRET_KEY\",
         \"near_account_public_key\": \"$NEAR_ACCOUNT_PUBLIC_KEY\",
         \"near_account_secret_key\": \"$NEAR_ACCOUNT_SECRET_KEY\",
-        \"url\": \"http://old-mpc-node-$1.service.mpc.consul:3000\"
+        \"url\": \"http://mpc-node-$1.service.mpc.consul:3000\"
     }" > configs/$1.json
 }
 
@@ -128,15 +111,14 @@ hex_public_key_to_json_byte_array() {
 ##########################################################################################
 
 
-near account create-account sponsor-by-faucet-service signer-$SUFFIX.testnet autogenerate-new-keypair save-to-legacy-keychain network-config testnet create
+near account create-account sponsor-by-faucet-service $SIGNER autogenerate-new-keypair save-to-legacy-keychain network-config testnet create
 if [ $REDEPLOY -eq 1 ]; then
-    near deploy "signer-$SUFFIX.testnet" "$(compile_contract)"
+    near deploy $SIGNER "$(compile_contract)"
 fi
-echo $PARTICIPANTS
 for i in $(seq 0 $((PARTICIPANTS - 1))); do
     generate_key $i
-    near account create-account sponsor-by-faucet-service mpc-test$i-$SUFFIX.testnet use-manually-provided-public-key $(jq -r '.near_account_public_key' configs/$i.json) network-config testnet create
-    # near account create-account mpc-test$i-$SUFFIX.testnet use-manually-provided-public-key $(jq -r '.near_account_public_key' configs/$i.json) network-config testnet create
+    # near account create-account sponsor-by-faucet-service mpc-test$i-$SUFFIX.testnet use-manually-provided-public-key $(jq -r '.near_account_public_key' configs/$i.json) network-config testnet create
+    near account create-account fund-myself mpc-test$i-$SUFFIX.testnet '0.1 NEAR' autogenerate-new-keypair save-to-legacy-keychain sign-as $SIGNER network-config testnet sign-with-legacy-keychain send
 done
 
 
@@ -152,36 +134,43 @@ CANDIDATES="{
         [ $((i + 1)) -lt $PARTICIPANTS ] && echo ","
     done)
 }"
-echo $CANDIDATES
-echo ""
-
-
-#     {
-#         account              = "mpc-test0-test-key-generation0011111111111.testnet"
-#         account_sk = "ed25519:3ZVYRDC785uX9NgzUt1tWqwimaKxK36ge9NVrEVpy75Mkqt2kx12efeduAfLw8Dx3nWPj82X9mzGeTxLbNXysEAW"
-#         cipher_pk            = "9b6a39f87edd287a54430a2a49ba17cc707a75ee85085fe00800a18a1794127b"
-#         cipher_sk  = "8425790a303989b72fbceee2dd81c5c054d81c21d27a850fda837b1504b045d1"
-#         sign_sk    = "ed25519:668vJprWzofjZRUJfsn58jyov8fXVWMLaacott6raWThLPt7SyzUWmEVyDz6tusPCZRWgLzjzhwMcsjuzJXuLSyE"
-#     },
-
-
-# TODO FINISH FORMATED CANDIDATES
-FORMATED_CANDIDATES="{
-    $(for i in $(seq 0 $((PARTICIPANTS - 1))); do
-        MPC_NAME=mpc-test$i-$SUFFIX.testnet
-        echo "{
-            account: \"$MPC_NAME\",
-            account_sk: \"$(jq -r '.near_account_secret_key' configs/$i.json)\",
-            cipher_pk: \"$(jq -r '.cipher_public_key' configs/$i.json)\",
-            cipher_sk: \"$(jq -r '.cipher_private_key' configs/$i.json)\",
-            sign_sk: \"$(jq -r '.sign_secret_key' configs/$i.json)\"
-        }"
-        [ $((i + 1)) -lt $PARTICIPANTS ] && echo ","
-    done)
-}, mpc_contract = \"signer-$SUFFIX.testnet\","
-echo $FORMATED_CANDIDATES
-echo ""
 
 INIT_ARGS=$(jq -n --argjson candidates "$CANDIDATES" --argjson threshold "$THRESHOLD" '{ "candidates": $candidates, "threshold": $threshold }')
 
-near call "signer-$SUFFIX.testnet" init "$INIT_ARGS" --use-account "signer-$SUFFIX.testnet"
+near call $SIGNER init "$INIT_ARGS" --use-account $SIGNER
+
+echo "The signer account is $SIGNER."
+echo "The signer account is required in the sign_request.sh script. Please retain it until the completion of the process."
+echo "The script has generated the secret_variables.tf file."
+echo "Please copy the file to the Terraform directory: infra-ops/provisioning/nomad/mpc/"
+echo "Adjust the infra-ops repository location and use the following command to apply the changes:"
+echo "cp secret_variables.tf <PATH_TO_REPOSITORY>/infra-ops/provisioning/nomad/mpc/"
+FORMATED_CANDIDATES="variable \"old_mpc_nodes\" {\n
+\tdescription = \"List of old MPC nodes with credentials\"\n
+  \ttype = list(object({\n
+    \taccount    = string\n
+    \taccount_sk = string\n
+    \tcipher_pk  = string\n
+    \tcipher_sk  = string\n
+    \tsign_sk    = string\n
+  \t}))\n
+  \tdefault = [\n
+    $(for i in $(seq 0 $((PARTICIPANTS - 1))); do
+        MPC_NAME=mpc-test$i-$SUFFIX.testnet
+        echo "\t\t{\n
+            \t\t\taccount: \"$MPC_NAME\",\n
+            \t\t\taccount_sk: \"$(jq -r '.near_account_secret_key' configs/$i.json)\",\n
+            \t\t\tcipher_pk: \"$(jq -r '.cipher_public_key' configs/$i.json)\",\n
+            \t\t\tcipher_sk: \"$(jq -r '.cipher_private_key' configs/$i.json)\",\n
+            \t\t\tsign_sk: \"$(jq -r '.sign_secret_key' configs/$i.json)\"\n
+        \t\t}"
+        [ $((i + 1)) -lt $PARTICIPANTS ] && echo ",\n"
+    done)
+    \t]\n
+}\n
+variable \"mpc_contract_signer\" {\n
+  \tdescription = \"Signer account id\"\n
+  \ttype        = string\n
+  \tdefault     = \"$SIGNER\"\n
+}"
+echo -e $FORMATED_CANDIDATES > secret_variables.tf
--- devnet/sign_request.sh
+++ devnet/sign_request.sh
@@ -1,6 +1,42 @@
 #!/bin/bash
-SOURCE="nearone-robin.testnet"
-SUFFIX="-test-key-generation0004"
+##########################################################################################
+# Function to display help
+##########################################################################################
+
+show_help() {
+    echo "Usage: $0 [options]"
+    echo "Options:"
+    echo "  -h, --help           Display this help message"
+    echo "  -s, --signer_id      Specify signer account id provided by depoloy.sh script"
+}
+
+##########################################################################################
+# Variable parsing block
+##########################################################################################
+
+# Check if an argument is provided
+if [ $# -eq 0 ]; then
+    echo "No arguments provided, signer_id is required"
+    show_help
+    exit 1
+fi
+
+while [ ! -z "$1" ]; do
+   if [[ "$1" == "--help" ]] || [[ "$1" == "-h" ]]; then
+      show_help; exit 0;
+   elif [[ $1 == "-s" ]] || [[ "$1" == "--signer_id" ]]; then
+      SIGNER="$2"
+      shift
+   else
+      echo "Incorrect input provided $1"
+      show_help; exit 1;
+   fi
+shift
+done
+
+##########################################################################################
+# Functions block
+##########################################################################################
 
 REQUEST=$(cat << EOF
 {
@@ -13,4 +49,4 @@ REQUEST=$(cat << EOF
 }
 EOF)
 
-near call "signer-6e3c4c3b-e824-4f83-aff8-35efa70e360e.testnet" --use-account signer-6e3c4c3b-e824-4f83-aff8-35efa70e360e.testnet sign "$REQUEST" --deposit-yocto 1 --gas 60000000000000
+# near call $SIGNER --use-account $SIGNER "$REQUEST" --deposit-yocto 1 --gas 60000000000000
