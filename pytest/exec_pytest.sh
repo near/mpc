93f1302e7648a2d6f53eab82a719bbae6a345705
kevindeforth <kevindeforth@users.noreply.github.com>
2025-01-03 17:55:39 +0100

    - add pytest to ci - add readme.md for pytest - add shell script for pytest - update .gitignore

---
+++ pytest/exec_pytest.sh
@@ -0,0 +1,166 @@
+#!/bin/bash
+
+# -------------------------------------------------------------------
+# Script Name: exec_pytest.sh
+# Description: Compiles nearcore, smart contract and mpc node,
+#              activates a virtual environment with the required python dependencies,
+#              saves output to `output.log`.
+#
+# Usage:
+# - Run the script with optional flags:
+#   --reset-submodules : Resets git submodules.
+#   --verbose          : Enables detailed log output.
+#
+# Example:
+#   bash exec_pytest.sh --reset-submodules --verbose
+#
+# Requirements:
+# - bash
+# - Git
+# - Python 3 with venv module
+#
+# -------------------------------------------------------------------
+
+TEST_FILES=(
+    "index_signature_request.py"
+    # Add more test files here
+)
+
+LOG_FILE="output.log"
+log_output() {
+    if [ "$VERBOSE" = true ]; then
+        "$@" 2>&1 | tee -a "$LOG_FILE"
+        local cmd_exit_code=${PIPESTATUS[0]} # Get the exit code of the first command in the pipeline
+        return "$cmd_exit_code"
+    else
+        # Progress indicator
+        local progress_pid
+        {
+            while :; do
+                printf "." >&2
+                sleep 5
+            done
+        } &
+
+        trap 'kill $progress_pid 2>/dev/null' EXIT
+
+        progress_pid=$!
+        local temp_stderr
+        temp_stderr=$(mktemp)
+        "$@" >"$LOG_FILE" 2> >(tee "$temp_stderr" >&2)
+        local cmd_exit_code=${PIPESTATUS[0]} # Get the exit code of the first command in the pipeline
+
+        kill "$progress_pid" 2>/dev/null
+        wait "$progress_pid" 2>/dev/null
+        cat "$temp_stderr" >>"$LOG_FILE"
+        rm -f "$temp_stderr"
+
+        trap - EXIT
+        return "$cmd_exit_code"
+    fi
+}
+
+if ! GIT_ROOT=$(git rev-parse --show-toplevel 2>/dev/null); then
+    printf "\nError: Not inside a Git repository.\n"
+    exit 1
+fi
+
+echo "Git repository root: $GIT_ROOT"
+PYTEST_DIR="$GIT_ROOT/pytest"
+VENV_DIR="$PYTEST_DIR/venv"
+LIB_DIR="$GIT_ROOT/libs"
+REQ_DIR="$LIB_DIR/nearcore/pytest/requirements.txt"
+
+RESET_SUBMODULES=false
+VERBOSE=false
+for arg in "$@"; do
+    case $arg in
+    --reset-submodules)
+        RESET_SUBMODULES=true
+        shift
+        ;;
+    --verbose)
+        VERBOSE=true
+        ;;
+    *)
+        printf "\nError: Unknown argument: %s, $arg\n"
+        exit 1
+        ;;
+    esac
+done
+
+# optional: reset submodules
+if [ $RESET_SUBMODULES == true ]; then
+    printf "\nRestting submodules"
+    if ! log_output git submodule foreach --recursive git reset --hard; then
+        echo "Error updating submodule"
+        exit 1
+    fi
+    if ! log_output git submodule foreach --recursive git clean -fdx; then
+        echo "Error updating submodule"
+        exit 1
+    fi
+    if ! log_output git submodule update --init --recursive --force; then
+        echo "Error updating submodule"
+        exit 1
+    fi
+fi
+
+printf "\nBuilding nearcore"
+if ! log_output bash -c "cd '$LIB_DIR/nearcore' && cargo build --quiet --color=always -p neard"; then
+    echo "Cargo failed to complete nearcore compilation"
+    exit 1
+fi
+
+printf "\nBuilding contract"
+if ! log_output bash -c "cd '$LIB_DIR/mpc/chain-signatures' && cargo build --quiet --color=always -p mpc-contract --target=wasm32-unknown-unknown --release"; then
+    echo "Cargo failed to compile contract"
+    exit 1
+fi
+
+printf "\nCopying contract"
+if ! log_output bash -c "cp '$LIB_DIR/mpc/target/wasm32-unknown-unknown/release/mpc_contract.wasm' '$LIB_DIR/mpc/chain-signatures/res/mpc_contract.wasm'"; then
+    echo "Failed to copy the contract"
+    exit 1
+fi
+
+printf "\nBuilding main node"
+if ! log_output bash -c "cd '$GIT_ROOT' && cargo build --quiet --color=always -p mpc-node --release"; then
+    echo "Cargo failed to complete mpc node compilation:"
+    exit 1
+fi
+
+printf "\nChecking if virtual environment exists"
+if [ ! -d "$VENV_DIR" ]; then
+    echo "Virtual enviroment not found. Creating new one."
+    cd "$PYTEST_DIR" || {
+        echo "Error: Directory $PYTEST_DIR not found"
+        exit 1
+    }
+    if ! log_output python3 -m venv venv; then
+        echo "Failed to create virtualenv"
+    fi
+fi
+
+printf "\nActivating virtual environment."
+if ! source "$VENV_DIR/bin/activate"; then
+    echo "Error: could not activate virtual environment. $VENV_DIR/bin/activate"
+    exit 1
+fi
+
+printf "\nInstalling requirements"
+if ! log_output pip install -r "$REQ_DIR"; then
+    echo: "Error: Failed to install requirements."
+    exit 1
+fi
+
+printf "\nExecuting tests"
+for test_file in "${TEST_FILES[@]}"; do
+    echo "Running test: $test_file"
+    if ! log_output python "$PYTEST_DIR/$test_file"; then
+        echo "Error: Test '$test_file' failed."
+        exit 1
+    else
+        printf "\nSuccess: Test '$test_file' passed.\n"
+    fi
+done
---
+++ pytest/readme.md
@@ -0,0 +1,41 @@
+# How to run the python test
+
+Simply run `exec_pytest.sh` (optinal flag `--verbose` and `--reset-submodules`) or execute the steps below with the current directory at root of the mpc git repo.
+
+1. Ensure submodules are clean and point to the correct commit. Use the following commands at your own risk:
+```bash
+git submodule foreach --recursive git reset --hard
+git submodule foreach --recursive git clean -fdx
+git submodule update --init --recursive --force
+```
+
+2. Build nearcore and main node:
+```bash
+# build nearcore:
+cd libs/nearcore && cargo build -p neard
+
+# build the contract:
+cd ../chain-signatures && cargo build -p mpc-contract --target=wasm32-unknown-unknown --release
+
+# copy the contract:
+cd .. && cp target/wasm32-unknown-unknown/release/mpc_contract.wasm chain-signatures/res/mpc_contract.wasm
+# build the main node
+cd ../.. && cargo build -p mpc-node --release
+```
+
+3. Set up virtualenv (optional, but recommended):
+```bash
+cd pytest && python3 -m venv venv
+
+# activate virtualenv:
+source venv/bin/activate
+
+# install requirements:
+pip install -r ../libs/nearcore/pytest/requirements.txt
+```
+
+4. Run pytest:
+```bash
+python index_signature_request.py 
+```
+
