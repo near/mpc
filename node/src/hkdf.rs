f09296d67ef314ef8ae883391eb7ba006ee8f3b6
kuksag <georgiy.kuksa@gmail.com>
2025-02-03 01:52:24 +0100

    Key Derivation Function for Frost

--- node/src/hkdf.rs
+++ node/src/hkdf.rs
@@ -3,7 +3,7 @@ use hex_literal::hex;
 use hkdf::Hkdf;
 use k256::{
     elliptic_curve::{bigint::ArrayEncoding, sec1::ToEncodedPoint, PrimeField},
-    AffinePoint, Scalar, U256,
+    AffinePoint, U256,
 };
 use near_indexer_primitives::types::AccountId;
 use sha3::{Digest, Sha3_256};
@@ -13,13 +13,13 @@ pub trait ScalarExt: Sized {
     fn from_bytes(bytes: [u8; 32]) -> Option<Self>;
 }
 
-impl ScalarExt for Scalar {
+impl ScalarExt for k256::Scalar {
     /// Returns nothing if the bytes are greater than the field size of Secp256k1.
     /// This will be very rare (probability around 1/2^224) with random bytes as the field size is
     /// 2^256 - 2^32 - 2^9 - 2^8 - 2^7 - 2^6 - 2^4 - 1
     fn from_bytes(bytes: [u8; 32]) -> Option<Self> {
         let bytes = U256::from_be_slice(bytes.as_slice());
-        Scalar::from_repr(bytes.to_be_byte_array()).into_option()
+        k256::Scalar::from_repr(bytes.to_be_byte_array()).into_option()
     }
 }
 
@@ -35,11 +35,11 @@ const SALT: [u8; 32] = hex!("328a47c2b8794445255c1647608df5db85c68bb0e7170abec53
 /// set of participants and the entropy
 pub fn derive_randomness(
     pk: AffinePoint,
-    msg_hash: Scalar,
+    msg_hash: k256::Scalar,
     big_r: AffinePoint,
     mut participants: Vec<ParticipantId>,
     entropy: [u8; 32],
-) -> Scalar {
+) -> k256::Scalar {
     // create a string containing (pk, msg_hash, big_r, sorted(participants))
     let pk_encoded_point = pk.to_encoded_point(true);
     let encoded_pk: &[u8] = pk_encoded_point.as_bytes();
@@ -62,7 +62,7 @@ pub fn derive_randomness(
     // initiate hkdf with the salt and with some `good' entropy
     let hk = Hkdf::<Sha3_256>::new(Some(&SALT), &entropy);
 
-    let mut delta: Scalar = Scalar::ZERO;
+    let mut delta: k256::Scalar = k256::Scalar::ZERO;
     // If the randomness created is 0 then we want to generate a new randomness
     while bool::from(delta.is_zero()) {
         // Generate randomization out of HKDF(entropy, pk, msg_hash, big_r, participants, nonce)
@@ -76,11 +76,11 @@ pub fn derive_randomness(
         hk.expand(&concatenation, &mut okm).unwrap();
 
         // derive the randomness delta
-        delta = match Scalar::from_bytes(okm) {
+        delta = match k256::Scalar::from_bytes(okm) {
             Some(delta) => delta,
             // if delta falls outside the field
             // probability is negligible: in the order of 1/2^224
-            None => Scalar::ZERO,
+            None => k256::Scalar::ZERO,
         }
     }
     delta
@@ -91,7 +91,7 @@ pub fn derive_randomness(
 // and stored signatures could still be verified.
 const TWEAK_DERIVATION_PREFIX: &str = "near-mpc-recovery v0.1.0 epsilon derivation:";
 
-pub fn derive_tweak(predecessor_id: &AccountId, path: &str) -> Scalar {
+pub fn derive_tweak(predecessor_id: &AccountId, path: &str) -> k256::Scalar {
     // ',' is ACCOUNT_DATA_SEPARATOR from nearcore that indicate the end
     // of the accound id in the trie key. We reuse the same constant to
     // indicate the end of the account id in derivation path.
@@ -101,21 +101,170 @@ pub fn derive_tweak(predecessor_id: &AccountId, path: &str) -> Scalar {
     let mut hasher = Sha3_256::new();
     hasher.update(derivation_path);
     let hash: [u8; 32] = hasher.finalize().into();
-    Scalar::from_bytes(hash).expect(
+    k256::Scalar::from_bytes(hash).expect(
         "Expected hash of derived key to be in the
         field of size 2^256 - 2^32 - 2^9 - 2^8 - 2^7 - 2^6 - 2^4 - 1 ",
     )
 }
 
 /// Derives a public key from a tweak and a master public key by computing PK + [tweak] G
-pub fn derive_public_key(public_key: AffinePoint, tweak: Scalar) -> AffinePoint {
+pub fn derive_public_key(public_key: AffinePoint, tweak: k256::Scalar) -> AffinePoint {
     (AffinePoint::GENERATOR * tweak + public_key).to_affine()
+}
+
+mod frost {
+    use frost_core::Group;
+    use std::collections::BTreeMap;
+    type Tweak = [u8; 32];
+
+    pub fn derive_secret_share(
+        secret_share: frost_ed25519::keys::SecretShare,
+        tweak: Tweak,
+    ) -> frost_ed25519::keys::SecretShare {
+        let tweak = curve25519_dalek::Scalar::from_bytes_mod_order(tweak);
+        frost_ed25519::keys::SecretShare::new(
+            secret_share.identifier().clone(),
+            derive_signing_share(secret_share.signing_share().clone(), tweak),
+            derive_vssc(secret_share.commitment().clone(), tweak),
+        )
+    }
+
+    pub fn derive_public_key_package(
+        pubkey_package: frost_ed25519::keys::PublicKeyPackage,
+        tweak: Tweak,
+    ) -> frost_ed25519::keys::PublicKeyPackage {
+        let tweak = curve25519_dalek::Scalar::from_bytes_mod_order(tweak);
+
+        let verifying_shares: BTreeMap<
+            frost_ed25519::Identifier,
+            frost_ed25519::keys::VerifyingShare,
+        > = pubkey_package
+            .verifying_shares()
+            .into_iter()
+            .map(|(&identifier, &share)| (identifier, derive_verifying_share(share, tweak)))
+            .collect();
+        let verifying_key: frost_ed25519::VerifyingKey =
+            derive_verifying_key(pubkey_package.verifying_key().clone(), tweak);
+        frost_ed25519::keys::PublicKeyPackage::new(verifying_shares, verifying_key)
+    }
+
+    fn derive_vssc(
+        vssc: frost_ed25519::keys::VerifiableSecretSharingCommitment,
+        tweak: curve25519_dalek::Scalar,
+    )
+        -> frost_ed25519::keys::VerifiableSecretSharingCommitment {
+        vssc // TODO
+    }
+
+    fn derive_signing_share(
+        signing_share: frost_ed25519::keys::SigningShare,
+        tweak: curve25519_dalek::Scalar,
+    ) -> frost_ed25519::keys::SigningShare {
+        frost_ed25519::keys::SigningShare::new(tweak + signing_share.to_scalar())
+    }
+
+    fn derive_verifying_share(
+        verifying_share: frost_ed25519::keys::VerifyingShare,
+        tweak: curve25519_dalek::Scalar,
+    ) -> frost_ed25519::keys::VerifyingShare {
+        let result =
+            frost_ed25519::Ed25519Group::generator() * tweak + verifying_share.to_element();
+        frost_ed25519::keys::VerifyingShare::new(result)
+    }
+
+    fn derive_verifying_key(
+        verifying_key: frost_ed25519::VerifyingKey,
+        tweak: curve25519_dalek::Scalar,
+    ) -> frost_ed25519::VerifyingKey {
+        let result = frost_ed25519::Ed25519Group::generator() * tweak + verifying_key.to_element();
+        frost_ed25519::VerifyingKey::new(result)
+    }
+}
+
+#[cfg(test)]
+mod frost_kdf_tests {
+    use std::collections::BTreeMap;
+    use aes_gcm::aead::rand_core::RngCore;
+    use rand::thread_rng;
+    
+    #[test]
+    #[should_panic(expected = "InvalidSecretShare { culprit: None }")] // TODO
+    fn proof_of_concept() {
+        let mut rng = thread_rng();
+        let max_signers = 2;
+        let min_signers = 2;
+        let (shares, pubkey_package) = frost_ed25519::keys::generate_with_dealer(
+            max_signers,
+            min_signers,
+            frost_ed25519::keys::IdentifierList::Default,
+            &mut rng,
+        )
+            .unwrap();
+
+        let mut tweak = [0u8; 32];
+        rng.fill_bytes(&mut tweak);
+
+        let derived_pubkey_package =
+            crate::hkdf::frost::derive_public_key_package(pubkey_package, tweak);
+        let derived_shares: BTreeMap<frost_ed25519::Identifier, frost_ed25519::keys::SecretShare> = shares
+            .into_iter()
+            .map(|(id, share)| {
+                (id, crate::hkdf::frost::derive_secret_share(share, tweak))
+            }).collect();
+
+
+        let mut derived_key_packages: BTreeMap<_, _> = BTreeMap::new();
+
+        for (identifier, secret_share) in derived_shares.clone() {
+
+            /* Fails on the following unwrap */
+            let key_package = frost_ed25519::keys::KeyPackage::try_from(secret_share).unwrap();
+
+            derived_key_packages.insert(identifier, key_package);
+        }
+
+        ///
+
+        let mut nonces_map = BTreeMap::new();
+        let mut commitments_map = BTreeMap::new();
+
+        for participant_index in 1..=min_signers {
+            let participant_identifier = participant_index.try_into().expect("should be nonzero");
+            let key_package = &derived_shares[&participant_identifier];
+            let (nonces, commitments) = frost_ed25519::round1::commit(
+                key_package.signing_share(),
+                &mut rng,
+            );
+            nonces_map.insert(participant_identifier, nonces);
+            commitments_map.insert(participant_identifier, commitments);
+        }
+
+        let mut signature_shares = BTreeMap::new();
+        let message = "message to sign".as_bytes();
+        let signing_package = frost_ed25519::SigningPackage::new(commitments_map, message);
+
+        for participant_identifier in nonces_map.keys() {
+            let key_package = &derived_key_packages[participant_identifier];
+            let nonces = &nonces_map[participant_identifier];
+            let signature_share = frost_ed25519::round2::sign(&signing_package, nonces, key_package).unwrap();
+            signature_shares.insert(*participant_identifier, signature_share);
+        }
+
+        let group_signature = frost_ed25519::aggregate(&signing_package, &signature_shares, &derived_pubkey_package).unwrap();
+
+        let is_signature_valid = derived_pubkey_package
+            .verifying_key()
+            .verify(message, &group_signature)
+            .is_ok();
+        assert!(is_signature_valid);
+    }
 }
 
 // Test with a couple of different input values the randomness are also different.
 #[cfg(test)]
 mod derive_tests {
     use super::*;
+    use k256::Scalar;
     use rand::{rngs::OsRng, seq::SliceRandom, thread_rng, Rng};
     use std::collections::HashSet;
 
