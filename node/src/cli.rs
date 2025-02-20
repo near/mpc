ca180ae4a6f0506e377df76519ce9f2d2ec23660
Patrick <patryk.beza@gmail.com>
2025-02-20 18:07:34 +0100

    refactor(audit): shorten CLI's function bodies (#215)
    * refactor(audit): shorten CLI's function bodies
    
    This commit addresses finding B1 from the Trail of Bits audit titled
    'Functions with too many lines.'
    
    * Address code review comments
    
    * Refactor mpc_client.rs: split long functions
    
    * Address the review comments

--- node/src/cli.rs
+++ node/src/cli.rs
@@ -5,7 +5,7 @@ use crate::config::{
 use crate::config::{ConfigFile, IndexerConfig};
 use crate::coordinator::Coordinator;
 use crate::db::SecretDB;
-use crate::indexer::real::spawn_real_indexer;
+use crate::indexer::{real::spawn_real_indexer, IndexerAPI};
 use crate::keyshare::KeyshareStorageFactory;
 use crate::p2p::testing::{generate_test_p2p_configs, PortSeed};
 use crate::tracking::{self, start_root_task};
@@ -89,8 +89,9 @@ pub struct StartCmd {
 
 impl StartCmd {
     async fn run(self) -> anyhow::Result<()> {
-        let home_dir = PathBuf::from(self.home_dir);
-        let secrets = SecretsConfig::from_cli(&self.secret_store_key_hex, self.p2p_private_key)?;
+        let home_dir = PathBuf::from(self.home_dir.clone());
+        let secrets =
+            SecretsConfig::from_cli(&self.secret_store_key_hex, self.p2p_private_key.clone())?;
         let config = load_config_file(&home_dir)?;
 
         let (indexer_handle, indexer_api) = spawn_real_indexer(
@@ -100,43 +101,15 @@ impl StartCmd {
             self.account_secret_key.clone(),
         );
 
-        let root_future = async move {
-            let root_task_handle = tracking::current_task();
-            let web_server = start_web_server(root_task_handle, config.web_ui.clone()).await?;
-            let _web_server = tracking::spawn_checked("web server", web_server);
+        let root_future = Self::create_root_future(
+            home_dir.clone(),
+            config.clone(),
+            secrets.clone(),
+            indexer_api,
+            self.gcp_keyshare_secret_id.clone(),
+            self.gcp_project_id.clone(),
+        );
 
-            let secret_db = SecretDB::new(&home_dir, secrets.local_storage_aes_key)?;
-
-            let keyshare_storage_factory = if let Some(secret_id) = self.gcp_keyshare_secret_id {
-                let Some(project_id) = self.gcp_project_id else {
-                    anyhow::bail!("GCP_PROJECT_ID must be specified to use GCP_KEYSHARE_SECRET_ID");
-                };
-                KeyshareStorageFactory::Gcp {
-                    project_id,
-                    secret_id,
-                }
-            } else {
-                KeyshareStorageFactory::Local {
-                    home_dir: home_dir.clone(),
-                    encryption_key: secrets.local_storage_aes_key,
-                }
-            };
-
-            let coordinator = Coordinator {
-                clock: Clock::real(),
-                config_file: config,
-                secrets,
-                secret_db,
-                keyshare_storage_factory,
-                indexer: indexer_api,
-                currently_running_job_name: Arc::new(Mutex::new(String::new())),
-            };
-            coordinator.run().await
-        };
-
-        // Spawn a one-thread runtime to run the coordinator.
-        // TODO(#156): we shouldn't actually need to do this, but for now we
-        // do because there is also an std::thread we need to join.
         let root_runtime = tokio::runtime::Builder::new_multi_thread()
             .enable_all()
             .worker_threads(1)
@@ -149,7 +122,7 @@ impl StartCmd {
             }
             anyhow::Ok(())
         });
-        // If either fails, the whole program fails.
+
         tokio::select! {
             res = root_task => {
                 res??;
@@ -159,6 +132,47 @@ impl StartCmd {
             }
         }
         Ok(())
+    }
+
+    async fn create_root_future(
+        home_dir: PathBuf,
+        config: ConfigFile,
+        secrets: SecretsConfig,
+        indexer_api: IndexerAPI,
+        gcp_keyshare_secret_id: Option<String>,
+        gcp_project_id: Option<String>,
+    ) -> anyhow::Result<()> {
+        let root_task_handle = tracking::current_task();
+        let web_server = start_web_server(root_task_handle, config.web_ui.clone()).await?;
+        let _web_server = tracking::spawn_checked("web server", web_server);
+
+        let secret_db = SecretDB::new(&home_dir, secrets.local_storage_aes_key)?;
+
+        let keyshare_storage_factory = if let Some(secret_id) = gcp_keyshare_secret_id {
+            let project_id = gcp_project_id.ok_or_else(|| {
+                anyhow::anyhow!("GCP_PROJECT_ID must be specified to use GCP_KEYSHARE_SECRET_ID")
+            })?;
+            KeyshareStorageFactory::Gcp {
+                project_id,
+                secret_id,
+            }
+        } else {
+            KeyshareStorageFactory::Local {
+                home_dir: home_dir.clone(),
+                encryption_key: secrets.local_storage_aes_key,
+            }
+        };
+
+        let coordinator = Coordinator {
+            clock: Clock::real(),
+            config_file: config,
+            secrets,
+            secret_db,
+            keyshare_storage_factory,
+            indexer: indexer_api,
+            currently_running_job_name: Arc::new(Mutex::new(String::new())),
+        };
+        coordinator.run().await
     }
 }
 
@@ -186,62 +200,92 @@ impl Cli {
                 None,
             ),
             Cli::GenerateTestConfigs {
-                output_dir,
-                participants,
+                ref output_dir,
+                ref participants,
                 threshold,
                 desired_triples_to_buffer,
                 desired_presignatures_to_buffer,
             } => {
-                let configs =
-                    generate_test_p2p_configs(&participants, threshold, PortSeed::CLI_FOR_PYTEST)?;
-                let participants_config = configs[0].0.participants.clone();
-                for (i, (_, p2p_private_key)) in configs.into_iter().enumerate() {
-                    let subdir = format!("{}/{}", output_dir, i);
-                    std::fs::create_dir_all(&subdir)?;
-                    let file_config = ConfigFile {
-                        my_near_account_id: participants[i].clone(),
-                        web_ui: WebUIConfig {
-                            host: "127.0.0.1".to_owned(),
-                            port: PortSeed::CLI_FOR_PYTEST.web_port(i),
-                        },
-                        indexer: IndexerConfig {
-                            validate_genesis: true,
-                            sync_mode: SyncMode::Block(BlockArgs { height: 0 }),
-                            concurrency: 1.try_into().unwrap(),
-                            mpc_contract_id: "test0".parse().unwrap(),
-                            finality: Finality::None,
-                            port_override: None,
-                        },
-                        triple: TripleConfig {
-                            concurrency: 2,
-                            desired_triples_to_buffer,
-                            timeout_sec: 60,
-                            parallel_triple_generation_stagger_time_sec: 1,
-                        },
-                        presignature: PresignatureConfig {
-                            concurrency: 2,
-                            desired_presignatures_to_buffer,
-                            timeout_sec: 60,
-                        },
-                        signature: SignatureConfig { timeout_sec: 60 },
-                        keygen: KeygenConfig { timeout_sec: 60 },
-                        cores: Some(4),
-                    };
-                    std::fs::write(
-                        format!("{}/p2p_key", subdir),
-                        SecretKey::ED25519(p2p_private_key).to_string(),
-                    )?;
-                    std::fs::write(
-                        format!("{}/config.yaml", subdir),
-                        serde_yaml::to_string(&file_config)?,
-                    )?;
-                }
-                std::fs::write(
-                    format!("{}/participants.json", output_dir),
-                    serde_json::to_string(&participants_config)?,
-                )?;
-                Ok(())
+                self.run_generate_test_configs(
+                    output_dir,
+                    participants,
+                    threshold,
+                    desired_triples_to_buffer,
+                    desired_presignatures_to_buffer,
+                )
+                .await
             }
         }
+    }
+
+    async fn run_generate_test_configs(
+        &self,
+        output_dir: &str,
+        participants: &[AccountId],
+        threshold: usize,
+        desired_triples_to_buffer: usize,
+        desired_presignatures_to_buffer: usize,
+    ) -> anyhow::Result<()> {
+        let configs = generate_test_p2p_configs(participants, threshold, PortSeed::CLI_FOR_PYTEST)?;
+        let participants_config = configs[0].0.participants.clone();
+        for (i, (_, p2p_private_key)) in configs.into_iter().enumerate() {
+            let subdir = format!("{}/{}", output_dir, i);
+            std::fs::create_dir_all(&subdir)?;
+            let file_config = self.create_file_config(
+                &participants[i],
+                i,
+                desired_triples_to_buffer,
+                desired_presignatures_to_buffer,
+            )?;
+            let secret_key = SecretKey::ED25519(p2p_private_key.clone());
+            std::fs::write(format!("{}/p2p_key", subdir), secret_key.to_string())?;
+            std::fs::write(
+                format!("{}/config.yaml", subdir),
+                serde_yaml::to_string(&file_config)?,
+            )?;
+        }
+        std::fs::write(
+            format!("{}/participants.json", output_dir),
+            serde_json::to_string(&participants_config)?,
+        )?;
+        Ok(())
+    }
+
+    fn create_file_config(
+        &self,
+        participant: &AccountId,
+        index: usize,
+        desired_triples_to_buffer: usize,
+        desired_presignatures_to_buffer: usize,
+    ) -> anyhow::Result<ConfigFile> {
+        Ok(ConfigFile {
+            my_near_account_id: participant.clone(),
+            web_ui: WebUIConfig {
+                host: "127.0.0.1".to_owned(),
+                port: PortSeed::CLI_FOR_PYTEST.web_port(index),
+            },
+            indexer: IndexerConfig {
+                validate_genesis: true,
+                sync_mode: SyncMode::Block(BlockArgs { height: 0 }),
+                concurrency: 1.try_into().unwrap(),
+                mpc_contract_id: "test0".parse().unwrap(),
+                finality: Finality::None,
+                port_override: None,
+            },
+            triple: TripleConfig {
+                concurrency: 2,
+                desired_triples_to_buffer,
+                timeout_sec: 60,
+                parallel_triple_generation_stagger_time_sec: 1,
+            },
+            presignature: PresignatureConfig {
+                concurrency: 2,
+                desired_presignatures_to_buffer,
+                timeout_sec: 60,
+            },
+            signature: SignatureConfig { timeout_sec: 60 },
+            keygen: KeygenConfig { timeout_sec: 60 },
+            cores: Some(4),
+        })
     }
 }
