7a91c3a5b5eae8c9e4a9358b80b6f5bc13f00022
Andrei <122784628+andrei-near@users.noreply.github.com>
2025-02-25 11:15:03 +0200

    mainnet env and script

--- scripts/update-mpc-node.sh
+++ scripts/update-mpc-node.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 CONTAINER_NAME="mpc-node"
-IMAGE_NAME="nearone/mpc-node-gcp:testnet-release"
+IMAGE_NAME="nearone/mpc-node-gcp:mainnet-release"
 ENV_FILE=".env"
 VOLUME_PATH="/home/mpc/data:/data"
 
