d94a287844820cdeb6a2a370eb999ff790814b36
Peter <petru.papucciu@nearone.org>
2025-03-19 08:52:22 +0000

    Chore: remove unused cipher keys from key generation

--- infra/scripts/generate_keys/src/main.rs
+++ infra/scripts/generate_keys/src/main.rs
@@ -1,12 +1,6 @@
-use mpc_keys::hpke;
 use rand::Rng;
 
 fn main() {
-    let (cipher_sk, cipher_pk) = hpke::generate();
-    let cipher_pk = hex::encode(cipher_pk.to_bytes());
-    let cipher_sk = hex::encode(cipher_sk.to_bytes());
-    println!("cipher public key: {}", cipher_pk);
-    println!("cipher private key: {}", cipher_sk);
     let sign_sk = near_crypto::SecretKey::from_random(near_crypto::KeyType::ED25519);
     let sign_pk = sign_sk.public_key();
     println!("p2p public key sign_pk: {}", sign_pk);
