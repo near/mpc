577a6ee1619fd05a45e24ac3f3e974933d457b4b
kevindeforth <kevindeforth@users.noreply.github.com>
2025-04-08 14:13:47 +0200

    improvements

---
+++ pytest/common_lib/signature.py
@@ -0,0 +1,49 @@
+import pathlib
+import sys
+import random
+import json
+import base64
+
+from typing import Dict, Literal, Optional
+
+from common_lib.contract_state import Domain, SignatureScheme
+
+sys.path.append(str(pathlib.Path(__file__).resolve().parents[1]))
+
+PayloadType = Literal["Ecdsa", "Eddsa"]
+
+signature_scheme_to_payload: Dict[SignatureScheme, PayloadType] = {
+    'Secp256k1': 'Ecdsa',
+    'Ed25519': 'Eddsa',
+}
+
+
+def generate_payload(payload_type: PayloadType) -> dict[PayloadType, str]:
+    return {payload_type: random.getrandbits(256).to_bytes(32, 'big').hex()}
+
+
+def generate_sign_args(
+        domain: Domain,
+        path: str = 'test',
+        payload: Optional[dict[PayloadType, str]] = None) -> dict:
+    if payload is None:
+        payload = generate_payload(signature_scheme_to_payload[domain.scheme])
+    return {
+        'request': {
+            'domain_id': domain.id,
+            'path': path,
+            'payload_v2': payload,
+        }
+    }
+
+
+def assert_signature_success(res):
+    try:
+        signature_base64 = res['result']['status']['SuccessValue']
+    except KeyError:
+        raise AssertionError(json.dumps(res, indent=1))
+
+    signature_base64 += '=' * ((4 - len(signature_base64) % 4) % 4)
+    signature = json.loads(base64.b64decode(signature_base64))
+    print("\033[96mSign Response ✓\033[0m")
+    return signature
