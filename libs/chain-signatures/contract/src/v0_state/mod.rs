f3e5da55ee4bf7de9ef64ee156e24c505cb496e9
kuksag <georgiy.kuksa@gmail.com>
2025-05-13 13:33:23 -0700

    Update migration logic

--- libs/chain-signatures/contract/src/v0_state/mod.rs
+++ libs/chain-signatures/contract/src/v0_state/mod.rs
@@ -1,59 +1,38 @@
-use std::collections::BTreeMap;
-
+use near_account_id::AccountId;
+use near_sdk::store::IterableMap;
 use near_sdk::{near, store::LookupMap};
+use std::collections::HashSet;
 
-use crate::{
-    config::Config,
-    primitives::{
-        domain::{AddDomainsVotes, DomainRegistry},
-        key_state::{AuthenticatedParticipantId, KeyForDomain, Keyset},
-        signature::{SignatureRequest, YieldIndex},
-        thresholds::ThresholdParameters,
-    },
-    state::{initializing::InitializingContractState, key_event::KeyEvent},
-    update::ProposedUpdates,
-};
+use crate::legacy_contract_state::ConfigV1;
+use crate::state::ProtocolContractState;
+use crate::update::UpdateId;
+use crate::{config::Config, primitives::signature::{SignatureRequest, YieldIndex}, AllowedCodeHashes, MpcContract, TeeState};
+use crate::primitives::code_hash::CodeHashesVotes;
 
-#[near(serializers=[borsh, json])]
-#[derive(Debug)]
-#[cfg_attr(feature = "dev-utils", derive(Clone))]
-pub struct ResharingContractState {
-    pub previous_running_state: RunningContractState,
-    pub reshared_keys: Vec<KeyForDomain>,
-    pub resharing_key: KeyEvent,
-}
-#[near(serializers=[borsh, json])]
-#[derive(Debug, Default, PartialEq)]
-#[cfg_attr(feature = "dev-utils", derive(Clone))]
-pub struct ThresholdParametersVotes {
-    proposal_by_account: BTreeMap<AuthenticatedParticipantId, ThresholdParameters>,
+#[allow(clippy::large_enum_variant)]
+#[near(serializers=[borsh])]
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
+pub enum Update {
+    Config(Config),
+    Contract(Vec<u8>),
+    ConfigV1(ConfigV1),
 }
 
-#[near(serializers=[borsh, json])]
+#[near(serializers=[borsh])]
 #[derive(Debug)]
-#[cfg_attr(feature = "dev-utils", derive(Clone))]
-pub struct RunningContractState {
-    /// The domains for which we have a key ready for signature processing.
-    pub domains: DomainRegistry,
-    /// The keys that are currently in use; for each domain provides an unique identifier for a
-    /// distributed key, so that the nodes can identify which local keyshare to use.
-    pub keyset: Keyset,
-    /// The current participants and threshold.
-    pub parameters: ThresholdParameters,
-    /// Votes for proposals for a new set of participants and threshold.
-    pub parameters_votes: ThresholdParametersVotes,
-    /// Votes for proposals to add new domains.
-    pub add_domains_votes: AddDomainsVotes,
+struct UpdateEntry {
+    updates: Vec<Update>,
+    votes: HashSet<AccountId>,
+    bytes_used: u128,
 }
-#[near(serializers=[borsh, json])]
+
+#[near(serializers=[borsh])]
 #[derive(Debug)]
-#[cfg_attr(feature = "dev-utils", derive(Clone))]
-pub enum ProtocolContractState {
-    NotInitialized,
-    Initializing(InitializingContractState),
-    Running(RunningContractState),
-    Resharing(ResharingContractState),
+pub struct ProposedUpdates {
+    entries: IterableMap<UpdateId, UpdateEntry>,
+    id: UpdateId,
 }
+
 #[near(serializers=[borsh])]
 #[derive(Debug)]
 pub struct MpcContractV0 {
@@ -61,4 +40,20 @@ pub struct MpcContractV0 {
     pub pending_requests: LookupMap<SignatureRequest, YieldIndex>,
     pub proposed_updates: ProposedUpdates,
     pub config: Config,
+}
+
+impl From<MpcContractV0> for MpcContract {
+    fn from(value: MpcContractV0) -> Self {
+        Self {
+            protocol_state: value.protocol_state.into(),
+            pending_requests: value.pending_requests,
+            proposed_updates: crate::update::ProposedUpdates::default(),
+            config: value.config,
+            tee_state: TeeState {
+                allowed_code_hashes: AllowedCodeHashes::default(),
+                historical_code_hashes: vec![],
+                votes: CodeHashesVotes::default(),
+            },
+        }
+    }
 }
