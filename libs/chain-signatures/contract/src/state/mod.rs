ecc4a967c8db3b57abf664377b46422dbb56dd5b
near-bookrock <near.polka513@slmail.me>
2025-05-26 17:44:09 +0200

    rename check function

--- libs/chain-signatures/contract/src/state/mod.rs
+++ libs/chain-signatures/contract/src/state/mod.rs
@@ -173,7 +173,7 @@ impl ProtocolContractState {
             ProtocolContractState::Resharing(_) => "Resharing",
         }
     }
-    pub fn is_running(&self) -> bool {
+    pub fn is_running_or_resharing(&self) -> bool {
         matches!(
             self,
             ProtocolContractState::Running(_) | ProtocolContractState::Resharing(_)
