a585241332604944536a9b586080026de88140ef
near-bookrock <near.polka513@slmail.me>
2025-06-02 16:15:28 +0200

    Add pytest for sign request during resharing

---
+++ pytest/tests/test_signature_request_during_resharing.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+"""
+Tests key resharing (adding and removing nodes).
+Starts 2 nodes, have node #3 join, then #4 join,
+then #1 leaves, and finally #2 leaves.
+At every step we check that signatures can still be produced.
+"""
+
+import pathlib
+import sys
+import time
+
+import pytest
+
+sys.path.append(str(pathlib.Path(__file__).resolve().parents[1]))
+from common_lib import shared
+from common_lib.contracts import load_mpc_contract
+
+
+def test_single_domain():
+    """
+    Tests that signature requests are still processed while performing key resharing.
+
+    Test scenario:
+        1. Start with 2 nodes and 1 domain.
+        2. Add 2 more nodes and start a key resharing.
+        3. While in resharing, kill one node such that the nodes are stuck in resharing.
+        4. Send a signature request to assert that it is processed while the network is in resharing state.
+    """
+    # Have the nodes disabled
+    cluster, mpc_nodes = shared.start_cluster_with_mpc(
+        2, 4, 1, load_mpc_contract(), start_mpc_nodes=True
+    )
+
+    cluster.init_cluster(participants=mpc_nodes[:2], threshold=2)
+
+    # Two new nodes join, increase threshold
+    cluster.do_resharing(
+        new_participants=mpc_nodes[:4],
+        new_threshold=3,
+        prospective_epoch_id=1,
+        wait_for_running=False,
+    )
+
+    # Kill one node such that resharing does not finish.
+    mpc_nodes[3].kill()
+
+    # sanity check
+    assert cluster.wait_for_state(
+        "Resharing"
+    ), "State should still be in resharing. 4th node was killed."
+
+    cluster.send_and_await_signature_requests(1)
+
+    # sanity check
+    assert cluster.wait_for_state(
+        "Resharing"
+    ), "State should still be in resharing. 4th node was killed."
