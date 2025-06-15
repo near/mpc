49bf11ed9d356dfdbc66007a62d8abb9cb02b8bb
kuksag <georgiy.kuksa@gmail.com>
2025-06-16 00:57:53 +0200

    Refactoring

--- pytest/common_lib/shared/__init__.py
+++ pytest/common_lib/shared/__init__.py
@@ -3,6 +3,7 @@ import os
 import pathlib
 import subprocess
 import sys
+from typing import List, Iterable, Optional
 
 import base58
 import yaml
@@ -15,13 +16,14 @@ from common_lib.shared.yaml_safeloader import SafeLoaderIgnoreUnknown
 
 sys.path.append(str(pathlib.Path(__file__).resolve().parents[1]))
 
-from cluster import start_cluster
+from cluster import start_cluster, LocalNode
 
 from transaction import create_create_account_action, create_payment_action, \
     create_full_access_key_action, sign_transaction, serialize_transaction
 
 from key import Key
 
+dot_near = pathlib.Path.home() / '.near'
 
 # Output is deserializable into the rust type near_crypto::SecretKey
 def serialize_key(key: Key):
@@ -29,28 +31,36 @@ def serialize_key(key: Key):
     return 'ed25519:' + base58.b58encode(full_key).decode('ascii')
 
 
-def sign_create_account_with_multiple_access_keys_tx(creator_key,
-                                                     new_account_id, keys,
-                                                     nonce, block_hash):
+def sign_create_account_with_multiple_access_keys_tx(
+        creator_key: Key,
+        new_account_id,
+        keys: List[Key],
+        nonce,
+        block_hash
+) -> bytes:
     create_account_action = create_create_account_action()
     payment_action = create_payment_action(100 * NEAR_BASE)
     access_key_actions = [
         create_full_access_key_action(key.decoded_pk()) for key in keys
     ]
     actions = [create_account_action, payment_action] + access_key_actions
-    signed_tx = sign_transaction(new_account_id, nonce, actions, block_hash,
-                                 creator_key.account_id,
-                                 creator_key.decoded_pk(),
-                                 creator_key.decoded_sk())
+    signed_tx = sign_transaction(
+        new_account_id,
+        nonce,
+        actions,
+        block_hash,
+        creator_key.account_id,
+        creator_key.decoded_pk(),
+        creator_key.decoded_sk()
+    )
     return serialize_transaction(signed_tx)
 
 
-def start_cluster_with_mpc(num_validators,
-                           num_mpc_nodes,
-                           num_respond_aks,
-                           contract,
-                           presignatures_to_buffer=None,
-                           start_mpc_nodes=True):
+def start_neard_cluster_with_cleanup(
+        num_validators: int,
+        num_mpc_nodes: int,
+        mpc_node_indices: Iterable[int]
+) -> List[LocalNode] :
     rpc_polling_config = {
         "rpc": {
             "polling_config": {
@@ -66,33 +76,107 @@ def start_cluster_with_mpc(num_validators,
         }
     }
 
-    # Start a near network with extra observer nodes; we will use their
-    # config.json, genesis.json, etc. to configure the mpc nodes' indexers
+    client_config_changes = {
+        0: rpc_polling_config,
+        1: rpc_polling_config
+    }
 
     nodes = start_cluster(
         num_validators, num_mpc_nodes, 1, None,
-        [["epoch_length", 1000], ["block_producer_kickout_threshold", 80]], {
-            0: rpc_polling_config,
-            1: rpc_polling_config
-        })
+        [["epoch_length", 1000], ["block_producer_kickout_threshold", 80]],
+        client_config_changes=client_config_changes
+    )
 
-    mpc_node_indices = range(num_validators, num_validators + num_mpc_nodes)
     for i in mpc_node_indices:
         nodes[i].kill(gentle=True)
         nodes[i].reset_data()
 
-    # Generate the mpc configs
-    dot_near = pathlib.Path.home() / '.near'
-    cmd = (MPC_BINARY_PATH, 'generate-test-configs', '--output-dir', dot_near,
-           '--participants', ','.join(f'test{i + num_validators}'
-                                      for i in range(num_mpc_nodes)),
-           '--threshold', str(num_mpc_nodes))
+    return nodes
+
+
+def generate_mpc_configs(
+        num_mpc_nodes: int,
+        num_validators: int,
+        presignatures_to_buffer: Optional[int]
+):
+    participants = ','.join(f'test{i + num_validators}'
+                            for i in range(num_mpc_nodes))
+    cmd = (
+        MPC_BINARY_PATH, 'generate-test-configs',
+        '--output-dir', dot_near,
+        '--participants', participants,
+        '--threshold', str(num_mpc_nodes)
+    )
     if presignatures_to_buffer:
         cmd = cmd + (
-            '--desired-presignatures-to-buffer',
-            str(presignatures_to_buffer),
+            '--desired-presignatures-to-buffer', str(presignatures_to_buffer),
         )
     subprocess.run(cmd)
+
+
+def create_and_dump_responder_config(
+        num_respond_aks: int,
+        mpc_node: MpcNode,
+        cluster: MpcCluster,
+):
+    if num_respond_aks == 0:
+        return
+    # FIXME: For whatever reason we can not get `last_block_hash` from non-validator node at this moment
+    last_block_hash = cluster.contract_node.last_block_hash()
+    account_id = f"respond.{mpc_node.account_id()}"
+    access_keys = [
+        Key.from_seed_testonly(account_id, seed=f"{s}")
+        for s in range(0, num_respond_aks)
+    ]
+    tx = sign_create_account_with_multiple_access_keys_tx(
+        mpc_node.signer_key(),
+        account_id,
+        access_keys,
+        1,
+        last_block_hash
+    )
+    cluster.contract_node.send_txn_and_check_success(tx)
+    respond_cfg = {
+        'account_id': account_id,
+        'access_keys': list(map(serialize_key, access_keys)),
+    }
+    fname = os.path.join(mpc_node.near_node.node_dir, 'respond.yaml')
+    with open(fname, "w") as file:
+        yaml.dump(respond_cfg, file, default_flow_style=False)
+
+
+def adjust_node_indexer(mpc_node: MpcNode):
+    # Indexer config must explicitly specify tracked shard
+    fname = os.path.join(mpc_node.near_node.node_dir, 'config.json')
+    with open(fname) as fd:
+        config_json = json.load(fd)
+    config_json['tracked_shards'] = [0]
+    with open(fname, 'w') as fd:
+        json.dump(config_json, fd, indent=2)
+    print(f"Wrote {fname} as config for node {mpc_node.account_id()}")
+
+
+def start_cluster_with_mpc(
+        num_validators,
+        num_mpc_nodes,
+        num_respond_aks,
+        contract,
+        presignatures_to_buffer=None,
+        start_mpc_nodes=True
+):
+    mpc_node_indices = range(num_validators, num_validators + num_mpc_nodes)
+
+    near_nodes = start_neard_cluster_with_cleanup(
+        num_validators,
+        num_mpc_nodes,
+        mpc_node_indices
+    )
+
+    generate_mpc_configs(
+        num_mpc_nodes,
+        num_validators,
+        presignatures_to_buffer
+    )
 
     # Get the participant set from the mpc configs.
     candidates = []
@@ -107,62 +191,36 @@ def start_cluster_with_mpc(num_validators,
         my_port = p['port']
 
         candidates.append({
-            "account_id":
-                near_account,
-            "cipher_pk": [
-                0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-            ],
+            "account_id": near_account,
             "p2p_public_key": p2p_public_key,
             "url": f"http://{my_addr}:{my_port}",
         })
+
     for i in mpc_node_indices:
         # Move the generated mpc configs
         mpc_config_dir = dot_near / str(i - num_validators)
         for fname in os.listdir(mpc_config_dir):
-            subprocess.run(('mv', os.path.join(mpc_config_dir,
-                                               fname), nodes[i].node_dir))
+            subprocess.run(('mv', os.path.join(mpc_config_dir, fname), near_nodes[i].node_dir))
 
     mpc_nodes = [
         MpcNode(
-            nodes[i],
+            near_nodes[i],
             candidates[i - num_validators]["url"],
             candidates[i - num_validators]["p2p_public_key"]
         )
         for i in mpc_node_indices
     ]
-    cluster = MpcCluster(near_nodes=[NearAccount(node) for node in nodes])
+    cluster = MpcCluster(near_nodes=[NearAccount(node) for node in near_nodes])
 
-    last_block_hash = cluster.contract_node.last_block_hash()
     # Set up the node's home directories
     for mpc_node in mpc_nodes:
-        # Indexer config must explicitly specify tracked shard
-        fname = os.path.join(mpc_node.near_node.node_dir, 'config.json')
-        with open(fname) as fd:
-            config_json = json.load(fd)
-        config_json['tracked_shards'] = [0]
-        with open(fname, 'w') as fd:
-            json.dump(config_json, fd, indent=2)
-        print(f"Wrote {fname} as config for node {mpc_node.account_id()}")
+        adjust_node_indexer(mpc_node)
 
-        # Create respond.yaml with credentials for sending responses
-        if num_respond_aks > 0:
-            account_id = f"respond.{mpc_node.account_id()}"
-            access_keys = [
-                Key.from_seed_testonly(account_id, seed=f"{s}")
-                for s in range(0, num_respond_aks)
-            ]
-            tx = sign_create_account_with_multiple_access_keys_tx(
-                mpc_node.signer_key(), account_id, access_keys, 1,
-                last_block_hash)
-            cluster.contract_node.send_txn_and_check_success(tx)
-            respond_cfg = {
-                'account_id': account_id,
-                'access_keys': list(map(serialize_key, access_keys)),
-            }
-            fname = os.path.join(mpc_node.near_node.node_dir, 'respond.yaml')
-            with open(fname, "w") as file:
-                yaml.dump(respond_cfg, file, default_flow_style=False)
+        create_and_dump_responder_config(
+            num_respond_aks,
+            mpc_node,
+            cluster
+        )
 
     # Deploy the mpc contract
     cluster.deploy_contract(contract)
