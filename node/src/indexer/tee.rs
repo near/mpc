c250bccfab14fe556d9b551280c01a2189748131
near-bookrock <near.polka513@slmail.me>
2025-06-18 13:52:12 +0200

    add exponential backoff

--- node/src/indexer/tee.rs
+++ node/src/indexer/tee.rs
@@ -1,5 +1,6 @@
-use std::sync::Arc;
+use std::{sync::Arc, time::Duration};
 
+use backon::{BackoffBuilder, ExponentialBuilder};
 use mpc_contract::tee::proposal::AllowedDockerImageHash;
 use tokio::sync::watch;
 
@@ -8,6 +9,11 @@ use crate::indexer::{
     IndexerState,
 };
 
+const ALLOWED_IMAGE_HASHES_REFRESH_INTERVAL: std::time::Duration =
+    std::time::Duration::from_secs(1);
+const MIN_BACKOFF_DURATION: Duration = Duration::from_secs(1);
+const MAX_BACKOFF_DURATION: Duration = Duration::from_secs(60);
+
 /// This future waits for the indexer to fully sync, and returns
 /// a [`watch::Receiver`] that will be continuously updated with the latest
 /// allowed [`AllowedDockerImageHash`]es when a change is detected
@@ -15,29 +21,40 @@ use crate::indexer::{
 pub async fn monitor_allowed_docker_images(
     indexer_state: Arc<IndexerState>,
 ) -> watch::Receiver<Vec<AllowedDockerImageHash>> {
-    const CONTRACT_STATE_REFRESH_INTERVAL: std::time::Duration = std::time::Duration::from_secs(1);
-
     tracing::debug!(target: "indexer", "awaiting full sync to read mpc contract state");
     wait_for_full_sync(&indexer_state.client).await;
 
     let fetch_allowed_image_hashes = {
-        async move || loop {
-            match get_mpc_tee_state(
-                indexer_state.mpc_contract_id.clone(),
-                &indexer_state.view_client,
-            )
-            .await
-            {
-                Ok((block_height, mut allowed_images_on_contract)) => {
-                    let allowed_images: Vec<AllowedDockerImageHash> =
-                        allowed_images_on_contract.get(block_height);
-                    break allowed_images;
-                }
-                Err(e) => {
-                    tracing::error!(target: "mpc", "error reading tee state from chain: {:?}", e);
-                    continue;
-                }
-            };
+        async move || {
+            let mut backoff = ExponentialBuilder::default()
+                .with_min_delay(MIN_BACKOFF_DURATION)
+                .with_max_delay(MAX_BACKOFF_DURATION)
+                .without_max_times()
+                .with_jitter()
+                .build();
+
+            loop {
+                match get_mpc_tee_state(
+                    indexer_state.mpc_contract_id.clone(),
+                    &indexer_state.view_client,
+                )
+                .await
+                {
+                    Ok((block_height, mut allowed_images_on_contract)) => {
+                        let allowed_images: Vec<AllowedDockerImageHash> =
+                            allowed_images_on_contract.get(block_height);
+                        break allowed_images;
+                    }
+                    Err(e) => {
+                        tracing::error!(target: "mpc", "error reading tee state from chain: {:?}", e);
+
+                        let backoff_duration = backoff.next().unwrap_or(MAX_BACKOFF_DURATION);
+                        tokio::time::sleep(backoff_duration).await;
+
+                        continue;
+                    }
+                };
+            }
         }
     };
 
@@ -46,7 +63,7 @@ pub async fn monitor_allowed_docker_images(
 
     actix::spawn(async move {
         loop {
-            tokio::time::sleep(CONTRACT_STATE_REFRESH_INTERVAL).await;
+            tokio::time::sleep(ALLOWED_IMAGE_HASHES_REFRESH_INTERVAL).await;
             let new_tee_state = fetch_allowed_image_hashes().await;
             sender.send_if_modified(|previous_state| {
                 if *previous_state != new_tee_state {
