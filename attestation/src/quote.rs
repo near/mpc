002e26751a733de6c6c28f0e31f4c9e4ab1181ad
Patrick <patryk.beza@gmail.com>
2025-07-14 12:01:23 +0200

    Update attestation/src/quote.rs
    Co-authored-by: Mårten Blankfors <marten@blankfors.se>

--- attestation/src/quote.rs
+++ attestation/src/quote.rs
@@ -1,7 +1,7 @@
 use dcap_qvl::quote::Quote as DcapQuote;
 
 /// TEE Remote Attestation Quote that proves the participant's identity.
-pub(crate) struct Quote(pub(crate) DcapQuote);
+pub struct Quote(DcapQuote);
 
 impl Quote {
     pub fn new(quote: DcapQuote) -> Self {
