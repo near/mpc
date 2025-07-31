b7beb486d318e0f441738dd0a49ea3ec5f4eab4d
RogerWhisper <kadian303@hotmail.com>
2025-08-01 01:22:47 +0530

    chore(lazy_static): replace lazy_static with LazyLock  (#660)
    Co-authored-by: gilcu3 <gilcu3@gmail.com>

--- node/src/metrics.rs
+++ node/src/metrics.rs
@@ -1,226 +1,201 @@
-use lazy_static::lazy_static;
+use std::sync::LazyLock;
 
-lazy_static! {
-    pub static ref MPC_NUM_TRIPLES_GENERATED: prometheus::IntCounter =
-        prometheus::register_int_counter!(
-            "mpc_num_triples_generated",
-            "Number of triples generated (including both owned and not owned)"
-        )
-        .unwrap();
-}
-
-lazy_static! {
-    pub static ref MPC_TRIPLES_GENERATION_TIME_ELAPSED: prometheus::Histogram =
+pub static MPC_NUM_TRIPLES_GENERATED: LazyLock<prometheus::IntCounter> = LazyLock::new(|| {
+    prometheus::register_int_counter!(
+        "mpc_num_triples_generated",
+        "Number of triples generated (including both owned and not owned)"
+    )
+    .unwrap()
+});
+pub static MPC_TRIPLES_GENERATION_TIME_ELAPSED: LazyLock<prometheus::Histogram> =
+    LazyLock::new(|| {
         near_o11y::metrics::try_create_histogram(
             "near_mpc_triples_generation_time_elapsed",
             "Time take to generate a batch of triples",
         )
-        .unwrap();
-}
+        .unwrap()
+    });
+pub static MPC_PRE_SIGNATURE_TIME_ELAPSED: LazyLock<prometheus::Histogram> = LazyLock::new(|| {
+    near_o11y::metrics::try_create_histogram(
+        "near_mpc_pre_signature_time_elapsed",
+        "Time take to generate a pre signature",
+    )
+    .unwrap()
+});
+pub static MPC_SIGNATURE_TIME_ELAPSED: LazyLock<prometheus::Histogram> = LazyLock::new(|| {
+    near_o11y::metrics::try_create_histogram(
+        "near_mpc_signature_time_elapsed",
+        "Time take to generate a signature",
+    )
+    .unwrap()
+});
+pub static MPC_OWNED_NUM_TRIPLES_AVAILABLE: LazyLock<prometheus::IntGauge> = LazyLock::new(|| {
+    prometheus::register_int_gauge!(
+        "mpc_owned_num_triples_available",
+        "Number of triples generated that we own, and not yet used"
+    )
+    .unwrap()
+});
 
-lazy_static! {
-    pub static ref MPC_PRE_SIGNATURE_TIME_ELAPSED: prometheus::Histogram =
-        near_o11y::metrics::try_create_histogram(
-            "near_mpc_pre_signature_time_elapsed",
-            "Time take to generate a pre signature",
-        )
-        .unwrap();
-}
-
-lazy_static! {
-    pub static ref MPC_SIGNATURE_TIME_ELAPSED: prometheus::Histogram =
-        near_o11y::metrics::try_create_histogram(
-            "near_mpc_signature_time_elapsed",
-            "Time take to generate a signature",
-        )
-        .unwrap();
-}
-
-lazy_static! {
-    pub static ref MPC_OWNED_NUM_TRIPLES_AVAILABLE: prometheus::IntGauge =
-        prometheus::register_int_gauge!(
-            "mpc_owned_num_triples_available",
-            "Number of triples generated that we own, and not yet used"
-        )
-        .unwrap();
-}
-
-lazy_static! {
-    pub static ref MPC_OWNED_NUM_TRIPLES_ONLINE: prometheus::IntGauge =
-        prometheus::register_int_gauge!(
-            "mpc_owned_num_triples_online",
-            "Number of triples generated that we own, and not yet used,
+pub static MPC_OWNED_NUM_TRIPLES_ONLINE: LazyLock<prometheus::IntGauge> = LazyLock::new(|| {
+    prometheus::register_int_gauge!(
+        "mpc_owned_num_triples_online",
+        "Number of triples generated that we own, and not yet used,
                 for which the participant set is confirmed alive"
-        )
-        .unwrap();
-}
+    )
+    .unwrap()
+});
 
-lazy_static! {
-    pub static ref MPC_OWNED_NUM_TRIPLES_WITH_OFFLINE_PARTICIPANT: prometheus::IntGauge =
+pub static MPC_OWNED_NUM_TRIPLES_WITH_OFFLINE_PARTICIPANT: LazyLock<prometheus::IntGauge> =
+    LazyLock::new(|| {
         prometheus::register_int_gauge!(
             "mpc_owned_num_triples_with_offline_participant",
             "Number of triples generated that we own, and not yet used,
                 for which some participant is offline",
         )
-        .unwrap();
-}
+        .unwrap()
+    });
 
-lazy_static! {
-    pub static ref MPC_OWNED_NUM_PRESIGNATURES_AVAILABLE: prometheus::IntGauge =
+pub static MPC_OWNED_NUM_PRESIGNATURES_AVAILABLE: LazyLock<prometheus::IntGauge> =
+    LazyLock::new(|| {
         prometheus::register_int_gauge!(
             "mpc_owned_num_presignatures_available",
             "Number of presignatures generated that we own, and not yet used"
         )
-        .unwrap();
-}
-
-lazy_static! {
-    pub static ref MPC_OWNED_NUM_PRESIGNATURES_ONLINE: prometheus::IntGauge =
+        .unwrap()
+    });
+pub static MPC_OWNED_NUM_PRESIGNATURES_ONLINE: LazyLock<prometheus::IntGauge> =
+    LazyLock::new(|| {
         prometheus::register_int_gauge!(
             "mpc_owned_num_presignatures_online",
             "Number of presignatures generated that we own, and not yet used,
                 for which the participant set is confirmed alive"
         )
-        .unwrap();
-}
-
-lazy_static! {
-    pub static ref MPC_OWNED_NUM_PRESIGNATURES_WITH_OFFLINE_PARTICIPANT: prometheus::IntGauge =
+        .unwrap()
+    });
+pub static MPC_OWNED_NUM_PRESIGNATURES_WITH_OFFLINE_PARTICIPANT: LazyLock<prometheus::IntGauge> =
+    LazyLock::new(|| {
         prometheus::register_int_gauge!(
             "mpc_owned_num_presignatures_with_offline_participant",
             "Number of presignatures generated that we own, and not yet used,
                 for which some participant is offline",
         )
-        .unwrap();
-}
-
-lazy_static! {
-    pub static ref MPC_INDEXER_NUM_RECEIPT_EXECUTION_OUTCOMES: prometheus::IntCounter =
+        .unwrap()
+    });
+pub static MPC_INDEXER_NUM_RECEIPT_EXECUTION_OUTCOMES: LazyLock<prometheus::IntCounter> =
+    LazyLock::new(|| {
         prometheus::register_int_counter!(
             "mpc_indexer_num_receipt_execution_outcomes",
             "Number of receipt execution outcomes processed by the near indexer"
         )
-        .unwrap();
-}
+        .unwrap()
+    });
 
-lazy_static! {
-    pub static ref MPC_NUM_SIGN_REQUESTS_INDEXED: prometheus::IntCounter =
-        prometheus::register_int_counter!(
-            "mpc_num_signature_requests_indexed",
-            "Number of signatures requests seen by the indexer"
-        )
-        .unwrap();
-}
+pub static MPC_NUM_SIGN_REQUESTS_INDEXED: LazyLock<prometheus::IntCounter> = LazyLock::new(|| {
+    prometheus::register_int_counter!(
+        "mpc_num_signature_requests_indexed",
+        "Number of signatures requests seen by the indexer"
+    )
+    .unwrap()
+});
 
-lazy_static! {
-    pub static ref MPC_NUM_SIGN_RESPONSES_INDEXED: prometheus::IntCounter =
-        prometheus::register_int_counter!(
-            "mpc_num_signature_responses_indexed",
-            "Number of signatures responses seen by the indexer"
-        )
-        .unwrap();
-}
-
-lazy_static! {
-    pub static ref MPC_NUM_SIGNATURE_COMPUTATIONS_LED: prometheus::IntCounterVec =
+pub static MPC_NUM_SIGN_RESPONSES_INDEXED: LazyLock<prometheus::IntCounter> = LazyLock::new(|| {
+    prometheus::register_int_counter!(
+        "mpc_num_signature_responses_indexed",
+        "Number of signatures responses seen by the indexer"
+    )
+    .unwrap()
+});
+pub static MPC_NUM_SIGNATURE_COMPUTATIONS_LED: LazyLock<prometheus::IntCounterVec> =
+    LazyLock::new(|| {
         prometheus::register_int_counter_vec!(
             "mpc_num_signature_computations_led",
             "Number of signature computations that this node led",
             &["result"],
         )
-        .unwrap();
-}
-
-lazy_static! {
-    pub static ref MPC_NUM_PASSIVE_SIGN_REQUESTS_RECEIVED: prometheus::IntCounter =
+        .unwrap()
+    });
+pub static MPC_NUM_PASSIVE_SIGN_REQUESTS_RECEIVED: LazyLock<prometheus::IntCounter> =
+    LazyLock::new(|| {
         prometheus::register_int_counter!(
             "mpc_num_passive_signature_requests_received",
             "Number of passive signature requests received from mpc peers"
         )
-        .unwrap();
-}
+        .unwrap()
+    });
 
-lazy_static! {
-    pub static ref MPC_NUM_PASSIVE_SIGN_REQUESTS_LOOKUP_SUCCEEDED: prometheus::IntCounter =
+pub static MPC_NUM_PASSIVE_SIGN_REQUESTS_LOOKUP_SUCCEEDED: LazyLock<prometheus::IntCounter> =
+    LazyLock::new(|| {
         prometheus::register_int_counter!(
             "mpc_num_passive_signature_requests_lookup_succeeded",
             "Number of passive signature requests successfully looked up in local DB"
         )
-        .unwrap();
-}
+        .unwrap()
+    });
 
-lazy_static! {
-    pub static ref MPC_OUTGOING_TRANSACTION_OUTCOMES: prometheus::IntCounterVec =
+pub static MPC_OUTGOING_TRANSACTION_OUTCOMES: LazyLock<prometheus::IntCounterVec> =
+    LazyLock::new(|| {
         prometheus::register_int_counter_vec!(
             "mpc_outgoing_transaction_outcomes",
             "Number of transactions sent by this node, by type and outcome",
             &["type", "outcome"],
         )
-        .unwrap();
-}
-
-lazy_static! {
-    pub static ref MPC_INDEXER_LATEST_BLOCK_HEIGHT: prometheus::IntGauge =
-        prometheus::register_int_gauge!(
-            "mpc_indexer_latest_block_height",
-            "Latest block height processed by the near indexer"
-        )
-        .unwrap();
-}
-
-lazy_static! {
-    pub static ref MPC_ACCESS_KEY_NONCE: prometheus::IntGauge = prometheus::register_int_gauge!(
+        .unwrap()
+    });
+pub static MPC_INDEXER_LATEST_BLOCK_HEIGHT: LazyLock<prometheus::IntGauge> = LazyLock::new(|| {
+    prometheus::register_int_gauge!(
+        "mpc_indexer_latest_block_height",
+        "Latest block height processed by the near indexer"
+    )
+    .unwrap()
+});
+#[allow(dead_code)]
+pub static MPC_ACCESS_KEY_NONCE: LazyLock<prometheus::IntGauge> = LazyLock::new(|| {
+    prometheus::register_int_gauge!(
         "mpc_access_key_nonce",
         "Latest observed nonce among transactions submitted using
              the node's access key for its near account",
     )
-    .unwrap();
-}
+    .unwrap()
+});
+pub static MPC_CURRENT_JOB_STATE: LazyLock<prometheus::IntGaugeVec> = LazyLock::new(|| {
+    prometheus::register_int_gauge_vec!(
+        "mpc_current_job_state",
+        "Current state of the top-level MPC job",
+        &["state"],
+    )
+    .unwrap()
+});
+pub static SIGN_REQUEST_CHANNEL_FAILED: LazyLock<prometheus::IntCounter> = LazyLock::new(|| {
+    prometheus::register_int_counter!(
+        "sign_request_channel_failed",
+        "failed to send on channel in sign_request_channel",
+    )
+    .unwrap()
+});
 
-lazy_static! {
-    pub static ref MPC_CURRENT_JOB_STATE: prometheus::IntGaugeVec =
-        prometheus::register_int_gauge_vec!(
-            "mpc_current_job_state",
-            "Current state of the top-level MPC job",
-            &["state"],
-        )
-        .unwrap();
-}
+pub static NETWORK_LIVE_CONNECTIONS: LazyLock<prometheus::IntGaugeVec> = LazyLock::new(|| {
+    prometheus::register_int_gauge_vec!(
+        "mpc_network_live_connections",
+        "Current state of the mesh network connections",
+        &["my_participant_id", "peer_participant_id"],
+    )
+    .unwrap()
+});
 
-lazy_static! {
-    pub static ref SIGN_REQUEST_CHANNEL_FAILED: prometheus::IntCounter =
-        prometheus::register_int_counter!(
-            "sign_request_channel_failed",
-            "failed to send on channel in sign_request_channel",
-        )
-        .unwrap();
-}
+pub static VERIFY_TEE_REQUESTS_SENT: LazyLock<prometheus::IntCounter> = LazyLock::new(|| {
+    prometheus::register_int_counter!(
+        "verify_tee_requests_sent",
+        "failed to send on channel in sign_request_channel",
+    )
+    .unwrap()
+});
 
-lazy_static! {
-    pub static ref NETWORK_LIVE_CONNECTIONS: prometheus::IntGaugeVec =
-        prometheus::register_int_gauge_vec!(
-            "mpc_network_live_connections",
-            "Current state of the mesh network connections",
-            &["my_participant_id", "peer_participant_id"],
-        )
-        .unwrap();
-}
-
-lazy_static! {
-    pub static ref VERIFY_TEE_REQUESTS_SENT: prometheus::IntCounter =
-        prometheus::register_int_counter!(
-            "verify_tee_requests_sent",
-            "failed to send on channel in sign_request_channel",
-        )
-        .unwrap();
-}
-
-lazy_static! {
-    pub static ref PEERS_INDEXER_HEIGHTS: prometheus::IntGaugeVec =
-        prometheus::register_int_gauge_vec!(
-            "mpc_peers_indexer_block_heights",
-            "Latest known block height of peers",
-            &["participant"]
-        )
-        .unwrap();
-}
+pub static PEERS_INDEXER_HEIGHTS: LazyLock<prometheus::IntGaugeVec> = LazyLock::new(|| {
+    prometheus::register_int_gauge_vec!(
+        "mpc_peers_indexer_block_heights",
+        "Latest known block height of peers",
+        &["participant"]
+    )
+    .unwrap()
+});
