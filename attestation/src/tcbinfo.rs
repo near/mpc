399ee5741a62cbde22d2dbd6fef81ae87ce5e293
Patryk Bęza <patryk.beza@gmail.com>
2025-08-01 14:24:57 +0200

    refactor(tee): move attestation generation logic from the attestation module
    Fixes #706

--- attestation/src/tcbinfo.rs
+++ attestation/src/tcbinfo.rs
@@ -1,6 +1,7 @@
 use derive_more::{Deref, From, Into};
 use dstack_sdk::dstack_client::TcbInfo as DstackTcbInfo;
+use serde::{Deserialize, Serialize};
 
 /// Dstack event log, a.k.a. the TCB Info.
-#[derive(From, Deref, Into)]
+#[derive(Serialize, Deserialize, From, Deref, Into)]
 pub struct TcbInfo(DstackTcbInfo);
