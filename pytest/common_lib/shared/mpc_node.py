a2094c1a521d9a33a9ec30017f49b6a33a3ac45f
kevindeforth <32777623+kevindeforth@users.noreply.github.com>
2025-08-05 12:00:16 +0200

    feat: export account balances as metric (#776)
    Co-authored-by: kevindeforth <kevindeforth@users.noreply.github.com>

--- pytest/common_lib/shared/mpc_node.py
+++ pytest/common_lib/shared/mpc_node.py
@@ -6,7 +6,7 @@ from key import Key
 from ruamel.yaml import YAML
 
 from common_lib.constants import LISTEN_BLOCKS_FILE, MPC_BINARY_PATH, TIMEOUT
-from common_lib.shared.metrics import DictMetricName, IntMetricName
+from common_lib.shared.metrics import DictMetricName, FloatMetricName, IntMetricName
 from common_lib.shared.near_account import NearAccount
 
 sys.path.append(str(pathlib.Path(__file__).resolve().parents[1]))
@@ -150,6 +150,13 @@ class MpcNode(NearAccount):
         file_path = file_path / LISTEN_BLOCKS_FILE
         print(f"setting {file_path} to {value}")
         file_path.write_text(str(value).lower())
+
+    def get_float_metric_value(self, metric: FloatMetricName) -> float | None:
+        """Helper function to return the float value of the metric."""
+        value = self.metrics.get_metric_value(metric, None)
+        if value is None:
+            return None
+        return float(value)
 
     def get_int_metric_value(self, metric: IntMetricName) -> int | None:
         return self.metrics.get_int_metric_value(metric)
