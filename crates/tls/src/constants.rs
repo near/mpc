e314d26a1e9db57c6a2f39e7faada47843de15a3
kevindeforth <kevindeforth@users.noreply.github.com>
2025-08-27 11:04:55 +0200

    cleanup

--- crates/tls/src/constants.rs
+++ crates/tls/src/constants.rs
@@ -8,5 +8,5 @@ MFECAQEwBQYDK2VwBCIEIGkMPQEb0GXxgFXbgojLebmHnCUpS3QYqJrYcfyFqHtW
 gSEAAbdC8KDpDZPqZalKndJm2N6EXn+cNxIb2gRa21P5mcs=
 -----END PRIVATE KEY-----";
 pub const SERVER_NAME: &str = "dummy";
-pub const TLS_PROTOCOL_VERSION: &'static rustls::SupportedProtocolVersion = &rustls::version::TLS13;
+pub const TLS_PROTOCOL_VERSION: &rustls::SupportedProtocolVersion = &rustls::version::TLS13;
 pub const ROOT_CERT: &str = "root";
