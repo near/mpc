634caa3b892cf4b9162f40bb119a118669b5feaf
Andrei <122784628+andrei-near@users.noreply.github.com>
2025-08-27 14:43:11 +0300

    revert

--- deployment/start.sh
+++ deployment/start.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 set -eo pipefail
 
-# This script is intended to be used for running nearone/mpc in a GCP environment.
+
+# This script is intended to be used for running nearone/mpc in a GCP environment. 
 # It will initialize the Near node in case it is not initialized yet and start the MPC node.
+
 
 MPC_NODE_CONFIG_FILE="$MPC_HOME_DIR/config.yaml"
 NEAR_NODE_CONFIG_FILE="$MPC_HOME_DIR/config.json"
 
 initialize_near_node() {
-    # boot_nodes must be filled in or else the node will not have any peers.
     ./mpc-node init --dir "$1" --chain-id "$MPC_ENV" --download-genesis --download-config --boot-nodes "$NEAR_BOOT_NODES"
 }
 
 update_near_node_config() {
-      python3 <<EOF
+    python3 <<EOF
 import json;
 config = json.load(open("$NEAR_NODE_CONFIG_FILE"))
 
-# boot nodes must be filled in or else the node will not have any peers.
+# boot nodes must be filled in or else the node will not be able to discover other nodes.
+config['network']['boot_nodes'] = "${NEAR_BOOT_NODES}"
 config['store']['load_mem_tries_for_tracked_shards'] = True
 config['state_sync']['sync']['ExternalStorage']['external_storage_fallback_threshold'] = 0
 
@@ -28,19 +30,9 @@ EOF
 }
 
 generate_mpc_config() {
-
-    if [ -n "$MPC_RESPONDER_ID" ]; then
-        responder_id="$MPC_RESPONDER_ID"
-    else
-        echo "WARNING: \$MPC_RESPONDER_ID is not set, falling back to \$MPC_ACCOUNT_ID"
-        responder_id="$MPC_ACCOUNT_ID"
-    fi
-
-    cat <<EOF >"$1"
+  cat <<EOF > "$1"
 # Configuration File
 my_near_account_id: $MPC_ACCOUNT_ID
-near_responder_account_id: $responder_id
-number_of_responder_keys: 50
 web_ui:
   host: 0.0.0.0
   port: 8080
@@ -77,22 +69,37 @@ fi
 # Update the Near node config with the MPC ENV variables values
 update_near_node_config && echo "Near node config updated"
 
-
 # Check and initialize MPC config if needed
 if [ -r "$MPC_NODE_CONFIG_FILE" ]; then
     echo "MPC node is already initialized but we'll still update the config"
 fi
 
-generate_mpc_config "$MPC_NODE_CONFIG_FILE" && echo "MPC node initialized"
-
+generate_mpc_config "$MPC_NODE_CONFIG_FILE" && echo "MPC nodeconfig initialized"
 
 # Check if MPC_SECRET_STORE_KEY is empty - if so, fetch from GCP Secret Manager
 if [ -z "${MPC_SECRET_STORE_KEY}" ]; then
-    echo "MPC_SECRET_STORE_KEY not provided in environment, will fetch from GCP Secret Manager..."
-    export MPC_SECRET_STORE_KEY=$(gcloud secrets versions access latest --project "$GCP_PROJECT_ID" --secret="$GCP_LOCAL_ENCRYPTION_KEY_SECRET_ID")
+  echo "MPC_SECRET_STORE_KEY not provided in environment, will fetch from GCP Secret Manager..."
+  export MPC_SECRET_STORE_KEY=$(gcloud secrets versions access latest --project $GCP_PROJECT_ID --secret=$GCP_LOCAL_ENCRYPTION_KEY_SECRET_ID)
 else
-    echo "Using provided MPC_SECRET_STORE_KEY from environment"
+  echo "Using provided MPC_SECRET_STORE_KEY from environment"
 fi
+
+# Check if MPC_P2P_PRIVATE_KEY is empty - if so, fetch from GCP Secret Manager
+if [ -z "${MPC_P2P_PRIVATE_KEY}" ]; then
+  echo "MPC_P2P_PRIVATE_KEY not provided in environment, will fetch from GCP Secret Manager..."
+  export MPC_P2P_PRIVATE_KEY=$(gcloud secrets versions access latest --project $GCP_PROJECT_ID --secret=$GCP_P2P_PRIVATE_KEY_SECRET_ID)
+else
+  echo "Using provided MPC_P2P_PRIVATE_KEY from environment"
+fi
+
+# Check if MPC_ACCOUNT_SK is empty - if so, fetch from GCP Secret Manager
+if [ -z "${MPC_ACCOUNT_SK}" ]; then
+  echo "MPC_ACCOUNT_SK not provided in environment, will fetch from GCP Secret Manager..."
+  export MPC_ACCOUNT_SK=$(gcloud secrets versions access latest --project $GCP_PROJECT_ID --secret=$GCP_ACCOUNT_SK_SECRET_ID)
+else
+  echo "Using provided MPC_ACCOUNT_SK from environment"
+fi
+
 
 echo "Starting mpc node..."
 /app/mpc-node start
