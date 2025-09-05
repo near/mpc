dda2378e5490c8264833df2e0af0b6a1212bd052
gilcu3 <gilcu3@gmail.com>
2025-09-05 16:07:49 +0200

    ruff!

--- pytest/tests/test_web_endpoints.py
+++ pytest/tests/test_web_endpoints.py
@@ -14,7 +14,9 @@ from common_lib.contracts import load_mpc_contract
 
 def test_web_endpoints():
     number_nodes = 2
-    cluster, mpc_nodes = shared.start_cluster_with_mpc(2, number_nodes, 1, load_mpc_contract())
+    cluster, mpc_nodes = shared.start_cluster_with_mpc(
+        2, number_nodes, 1, load_mpc_contract()
+    )
     cluster.init_cluster(participants=mpc_nodes, threshold=2)
     cluster.send_and_await_signature_requests(1)
     cluster.send_and_await_ckd_requests(1)
