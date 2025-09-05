1a4dce76b1ef1acb1413f600f0c032d8e28b21ff
Mårten Blankfors <marten@blankfors.se>
2025-09-05 16:45:44 +0200

    perf: Use hashes instead for RPC tests

--- crates/contract_history/src/lib.rs
+++ crates/contract_history/src/lib.rs
@@ -15,31 +15,32 @@ pub const fn version_2_2_0() -> &'static [u8; 566653] {
 mod tests {
     use super::*;
 
-    use base64::{Engine as _, prelude::BASE64_STANDARD};
+    use sha2::Digest;
 
     #[tokio::test]
     async fn mainnet_contract_should_be_up_to_date() {
         let on_chain_right_now =
-            fetch_contract_code("https://rpc.mainnet.near.org", "v1.signer").await;
+            fetch_contract_code_hash("https://rpc.mainnet.near.org", "v1.signer").await;
 
-        assert_eq!(current_mainnet(), on_chain_right_now);
+        assert_eq!(hash(current_mainnet()), on_chain_right_now);
     }
 
     #[tokio::test]
     async fn testnet_contract_should_be_up_to_date() {
         let on_chain_right_now =
-            fetch_contract_code("https://rpc.testnet.near.org", "v1.signer-prod.testnet").await;
+            fetch_contract_code_hash("https://rpc.testnet.near.org", "v1.signer-prod.testnet")
+                .await;
 
-        assert_eq!(current_testnet(), on_chain_right_now);
+        assert_eq!(hash(current_testnet()), on_chain_right_now);
     }
 
-    async fn fetch_contract_code(rpc_endpoint: &str, account_id: &str) -> Vec<u8> {
+    async fn fetch_contract_code_hash(rpc_endpoint: &str, account_id: &str) -> [u8; 32] {
         let body = serde_json::json!({
             "jsonrpc": "2.0",
             "id": "dontcare",
             "method": "query",
             "params": {
-                "request_type": "view_code",
+                "request_type": "view_account",
                 "finality": "final",
                 "account_id": account_id
             }
@@ -56,9 +57,17 @@ mod tests {
             .await
             .expect("contract response should be valid JSON");
 
-        BASE64_STANDARD
-            .decode(&response.result.code_base64)
-            .expect("response should be base64 encoded")
+        bs58::decode(&response.result.code_hash)
+            .into_vec()
+            .expect("response should be base58 encoded")
+            .try_into()
+            .expect("should be 32 bytes")
+    }
+
+    fn hash(code: &[u8]) -> [u8; 32] {
+        let mut hasher = sha2::Sha256::new();
+        hasher.update(code);
+        hasher.finalize().into()
     }
 
     #[derive(serde::Deserialize)]
@@ -68,6 +77,6 @@ mod tests {
 
     #[derive(serde::Deserialize)]
     struct RpcResult {
-        code_base64: String,
+        code_hash: String,
     }
 }
