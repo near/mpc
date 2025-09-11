3fbe94365fa89e394ec84879d0a5198964c791aa
gilcu3 <gilcu3@gmail.com>
2025-09-11 09:43:05 +0200

    include folders

--- deployment/build-images.sh
+++ deployment/build-images.sh
@@ -33,7 +33,7 @@ fi
 # this is necessary to fix reproducibility with old docker versions where
 # rewrite-timestamp is not working as expected
 # https://github.com/moby/buildkit/issues/4986
-find "." -type f -exec touch -d @"$SOURCE_DATE" {} +
+find . \( -type f -o -type d \) -exec touch -d @"$SOURCE_DATE" {} +
 
 
 buildkit_version="0.24.0"
