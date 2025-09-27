3d717a576fe94fc5c3f283675fa06f0689faa95f
Daniel Sharifi <me@dsharifi.com>
2025-09-27 12:56:50 +0000

    rename to upgrade_to_current_contract

--- crates/contract/tests/sandbox/mod.rs
+++ crates/contract/tests/sandbox/mod.rs
@@ -1,9 +1,9 @@
 pub mod ckd;
-pub mod contract_upgrade;
 pub mod integration_tee_cleanup_after_resharing;
 pub mod sign;
 pub mod tee;
 pub mod updates;
+pub mod upgrade_to_current_contract;
 pub mod vote;
 
 pub mod common;
