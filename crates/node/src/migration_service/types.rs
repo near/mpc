497b7640c5eab267a88e266a2ae80b326fa283bd
kevindeforth <kevindeforth@users.noreply.github.com>
2025-10-08 15:31:28 +0200

    test migration status constructor

--- crates/node/src/migration_service/types.rs
+++ crates/node/src/migration_service/types.rs
@@ -16,7 +16,17 @@ impl MigrationInfo {
         my_p2p_tls_key: &VerifyingKey,
         contract_state: &ContractMigrationInfo,
     ) -> Self {
-        infer_migration_info(my_account_id, my_p2p_tls_key, contract_state)
+        let (backup_service_info, active_migration) = match contract_state.get(my_account_id) {
+            Some((backup_service_info, destination_node_info)) => (
+                backup_service_info.clone(),
+                infer_migration_status(my_p2p_tls_key, destination_node_info),
+            ),
+            None => (None, false),
+        };
+        Self {
+            backup_service_info,
+            active_migration,
+        }
     }
 }
 
@@ -36,20 +46,69 @@ fn infer_migration_status(
         .unwrap_or(false)
 }
 
-fn infer_migration_info(
-    my_account_id: &AccountId,
-    my_p2p_tls_key: &VerifyingKey,
-    contract_state: &ContractMigrationInfo,
-) -> MigrationInfo {
-    let (backup_service_info, active_migration) = match contract_state.get(my_account_id) {
-        Some((backup_service_info, destination_node_info)) => (
-            backup_service_info.clone(),
-            infer_migration_status(my_p2p_tls_key, destination_node_info),
-        ),
-        None => (None, false),
+#[cfg(test)]
+mod tests {
+    use mpc_contract::{
+        node_migrations::{BackupServiceInfo, DestinationNodeInfo},
+        primitives::test_utils::{
+            bogus_ed25519_near_public_key, bogus_ed25519_public_key, gen_participant,
+        },
     };
-    MigrationInfo {
-        backup_service_info,
-        active_migration,
+
+    use crate::{indexer::migrations::ContractMigrationInfo, providers::PublicKeyConversion};
+
+    use super::MigrationInfo;
+
+    #[test]
+    fn test_migration_status_constructor_empty() {
+        let state = ContractMigrationInfo::new();
+        let (account_id, _) = gen_participant(0);
+        let signer_account_pk = bogus_ed25519_near_public_key();
+        let p2p_public_key =
+            ed25519_dalek::VerifyingKey::from_near_sdk_public_key(&signer_account_pk).unwrap();
+
+        let res = MigrationInfo::from_contract_state(&account_id, &p2p_public_key, &state);
+        assert!(!res.active_migration);
+        assert_eq!(res.backup_service_info, None);
+    }
+
+    #[test]
+    fn test_migration_status_constructor_populated() {
+        let mut state = ContractMigrationInfo::new();
+        let (account_id_0, participant_info_0) = gen_participant(0);
+        let (account_id_1, _) = gen_participant(1);
+        let signer_account_pk = bogus_ed25519_near_public_key();
+        let destination_node_info = DestinationNodeInfo {
+            signer_account_pk: signer_account_pk.clone(),
+            destination_node_info: participant_info_0.clone(),
+        };
+
+        let backup_service_info = BackupServiceInfo {
+            public_key: bogus_ed25519_public_key(),
+        };
+        state.insert(
+            account_id_1.clone(),
+            (
+                Some(backup_service_info.clone()),
+                Some(destination_node_info.clone()),
+            ),
+        );
+        let participating_key =
+            ed25519_dalek::VerifyingKey::from_near_sdk_public_key(&participant_info_0.sign_pk)
+                .unwrap();
+        let non_participating_key =
+            ed25519_dalek::VerifyingKey::from_near_sdk_public_key(&signer_account_pk).unwrap();
+
+        let res = MigrationInfo::from_contract_state(&account_id_0, &participating_key, &state);
+        assert!(!res.active_migration);
+        assert_eq!(res.backup_service_info, None);
+
+        let res = MigrationInfo::from_contract_state(&account_id_1, &non_participating_key, &state);
+        assert!(!res.active_migration);
+        assert_eq!(res.backup_service_info, Some(backup_service_info.clone()));
+
+        let res = MigrationInfo::from_contract_state(&account_id_1, &participating_key, &state);
+        assert!(res.active_migration);
+        assert_eq!(res.backup_service_info, Some(backup_service_info.clone()));
     }
 }
