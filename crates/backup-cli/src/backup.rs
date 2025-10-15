9ca0e8788a0c7ce85485a6a1ced054a642714c12
Patryk Bęza <patryk.beza@gmail.com>
2025-10-15 13:37:45 +0200

    WIP

--- crates/backup-cli/src/backup.rs
+++ crates/backup-cli/src/backup.rs
@@ -20,19 +20,44 @@ where
         self.secrets_storage
             .store_private_key(&private_key)
             .await
-            .expect("todo: Error handling");
+            .expect("fail to store private key");
     }
 
     /// Put backup service data to the smart contract
     pub async fn register_backup_service(&self) {
-        todo!();
+        let public_key = self
+            .secrets_storage
+            .load_private_key()
+            .await
+            .expect("fail to load private key")
+            .public_key();
+        self.mpc_contract
+            .register_backup_data(&public_key)
+            .await
+            .expect("fail to register backup data");
     }
 
     pub async fn get_keyshares(&self) {
-        todo!();
+        let keyshare = self
+            .mpc_p2p_client
+            .get_key_shares()
+            .await
+            .expect("fail to get key shares");
+        self.key_shares_storage
+            .store_key_shares(&keyshare)
+            .await
+            .expect("fail to store key shares");
     }
 
     pub async fn put_keyshares(&self) {
-        todo!();
+        let key_shares = self
+            .key_shares_storage
+            .load_key_shares()
+            .await
+            .expect("fail to load key shares");
+        self.mpc_p2p_client
+            .put_key_shares(&key_shares)
+            .await
+            .expect("fail to put key shares");
     }
 }
--- crates/backup-cli/src/types.rs
+++ crates/backup-cli/src/types.rs
@@ -1,3 +1,9 @@
 pub struct PrivateKey {}
 pub struct PublicKey {}
 pub struct KeyShares {}
+
+impl PrivateKey {
+    pub fn public_key(&self) -> PublicKey {
+        PublicKey {}
+    }
+}
