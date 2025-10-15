267d938c92f3f2ef8358b540c0b3140c352a8b14
Mårten Blankfors <marten@blankfors.se>
2025-10-15 14:47:18 +0200

    wip: Another arg we don't use :)

--- crates/backup-cli/src/cli.rs
+++ crates/backup-cli/src/cli.rs
@@ -6,6 +6,8 @@ pub struct Args {
     #[arg(long, env)]
     pub mpc_node_url: String,
     #[arg(long, env)]
+    pub mpc_node_p2p_key: String,
+    #[arg(long, env)]
     pub mpc_contract_name: String,
     #[arg(long, env)]
     pub near_network: String,
