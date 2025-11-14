c68fb5a75f451ad8184ff188b41dbe0fa0768d8f
gilcu3 <gilcu3@gmail.com>
2025-11-14 17:32:17 +0100

    fix: pytest fixtures are tricky

--- pytest/tests/conftest.py
+++ pytest/tests/conftest.py
@@ -168,25 +168,6 @@ def compile_parallel_contract(request):
     )
 
 
-@pytest.fixture(scope="package")
-def shared_cluster():
-    """
-    Spins up a cluster with three nodes, initializes the contract and adds domains. Returns the cluster in a running state.
-    """
-    cluster, mpc_nodes = shared.start_cluster_with_mpc(
-        2,
-        2,
-        1,
-        contracts.load_mpc_contract(),
-    )
-    cluster.init_cluster(mpc_nodes, 2)
-    cluster.wait_for_state(contract_state.ProtocolState.RUNNING)
-
-    yield cluster
-
-    atexit._run_exitfuncs()
-
-
 def git_root() -> Path:
     git_repo = git.Repo(".", search_parent_directories=True)
     return Path(git_repo.git.rev_parse("--show-toplevel"))
