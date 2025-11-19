47d081d387fbb2431205ecc7911d28ad07542bdb
kevindeforth <kevindeforth@users.noreply.github.com>
2025-11-19 17:18:40 +0100

    contract getter for proposed updates

---
+++ crates/contract-interface/src/types/primitives.rs
@@ -0,0 +1,22 @@
+use borsh::{BorshDeserialize, BorshSerialize};
+use serde::{Deserialize, Serialize};
+
+/// A Near AccountId
+#[derive(
+    Debug,
+    Clone,
+    Eq,
+    PartialEq,
+    Ord,
+    PartialOrd,
+    Hash,
+    Serialize,
+    Deserialize,
+    BorshSerialize,
+    BorshDeserialize,
+)]
+#[cfg_attr(
+    all(feature = "abi", not(target_arch = "wasm32")),
+    derive(schemars::JsonSchema)
+)]
+pub struct AccountId(pub String);
