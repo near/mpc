7657a74950bc29fe450f9a3915a681776822ac77
kevindeforth <kevindeforth@users.noreply.github.com>
2025-11-28 15:07:54 +0100

    .

--- crates/contract/src/primitives/votes/impls.rs
+++ crates/contract/src/primitives/votes/impls.rs
@@ -1,19 +1,36 @@
 use std::collections::BTreeSet;
 
-use anyhow;
+use near_sdk::{store::IterableMap, IntoStorageKey};
 
-use super::types::{ProposalBounds, ProposalIdBounds, VoterIdBounds, Votes};
+use super::types::{ProposalBounds, ProposalEntry, ProposalId, VoterIdBounds, Votes};
 
-impl<VoterId, ProposalId, Proposal> Votes<VoterId, ProposalId, Proposal>
+impl<VoterId, Proposal> Votes<VoterId, Proposal>
 where
     VoterId: VoterIdBounds,
-    ProposalId: ProposalIdBounds,
     Proposal: ProposalBounds,
 {
+    pub fn new<S>(votes_key: S, proposals_key: S) -> Self
+    where
+        S: IntoStorageKey,
+    {
+        Self {
+            votes: IterableMap::new(votes_key),
+            proposals: IterableMap::new(proposals_key),
+            next_id: 0.into(),
+        }
+    }
     /// returns the `ProposalId`
     pub fn propose(&mut self, proposal: Proposal) -> ProposalId {
-        self.next_id.clone()
+        let proposal_id = self.next_id.clone();
+        let proposal_entry = ProposalEntry {
+            proposed: proposal,
+            num_votes: 0,
+        };
+        self.proposals.insert(proposal_id.clone(), proposal_entry);
+        self.next_id += 1;
+        proposal_id
     }
+
     //    /// returns the number of votes for [`ProposalId`]
     pub fn vote(&mut self, voter_id: VoterId, proposal_id: ProposalId) -> u64 {
         0
@@ -27,4 +44,70 @@ where
     pub fn clear(&mut self) {}
     /// removes any vote not belonging to an element in [`BTreeSet<VoterId>`]
     pub fn retain_votes(voter_ids_to_keep: BTreeSet<VoterId>) {}
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::BTreeMap;
+
+    use borsh::BorshSerialize;
+    use near_sdk::{store::IterableMap, BorshStorageKey};
+
+    use crate::primitives::votes::types::{ProposalBounds, ProposalEntry, VoterIdBounds, Votes};
+
+    type VoterId = u64;
+    type Proposal = String;
+    impl VoterIdBounds for u64 {}
+    impl ProposalBounds for String {}
+
+    #[derive(BorshStorageKey, BorshSerialize)]
+    enum StorageKeys {
+        VotesStorageKey,
+        ProposalsStorageKey,
+    }
+
+    #[test]
+    fn test_votes_constructor() {
+        let votes: Votes<VoterId, Proposal> = Votes::new(
+            StorageKeys::VotesStorageKey,
+            StorageKeys::ProposalsStorageKey,
+        );
+        assert_eq!(votes.next_id, 0.into());
+        assert!(votes.proposals.is_empty());
+        assert!(votes.votes.is_empty());
+    }
+
+    #[test]
+    fn test_votes_propose_basic() {
+        let expected_proposal_id = 42u64;
+        let mut votes = Votes::<VoterId, Proposal> {
+            votes: IterableMap::new(StorageKeys::VotesStorageKey),
+            proposals: IterableMap::new(StorageKeys::ProposalsStorageKey),
+            next_id: expected_proposal_id.into(),
+        };
+
+        let proposal: Proposal = "hello world".into();
+        let proposal_id = votes.propose(proposal.clone());
+        assert_eq!(proposal_id.0, expected_proposal_id);
+        assert_eq!(votes.next_id.0, expected_proposal_id + 1);
+        assert!(votes.votes.is_empty());
+        assert_eq!(votes.proposals.len(), 1);
+        let ProposalEntry {
+            proposed,
+            num_votes,
+        } = votes.proposals.get(&proposal_id).unwrap();
+        assert_eq!(*proposed, proposal);
+        assert_eq!(*num_votes, 0);
+    }
+
+    struct ExpectedVotes {
+        votes: BTreeMap<VoterId, Proposal>,
+        proposals: BTreeMap<u64, ProposalEntry<Proposal>>,
+        next_id: u64,
+    }
+    // make above, but more complicated: generate random state and give expected value
+    fn gen_random_state() -> (Votes<VoterId, Proposal>, ExpectedVotes) {
+        let mut rng = rand::thread_rng();
+        let x: usize = rng.gen_range(0..=10);
+    }
 }
--- crates/contract/src/primitives/votes/types.rs
+++ crates/contract/src/primitives/votes/types.rs
@@ -1,20 +1,35 @@
+use std::ops::AddAssign;
+
 use borsh::{BorshDeserialize, BorshSerialize};
-use near_sdk::store::IterableMap;
+use derive_more::From;
+use near_sdk::{near, store::IterableMap};
 
 pub(crate) trait VoterIdBounds: BorshSerialize + Ord {}
-pub(crate) trait ProposalIdBounds: BorshSerialize + BorshDeserialize + Ord + Clone {}
+//pub(crate) trait ProposalIdBounds:
+//    BorshSerialize + BorshDeserialize + Ord + Clone + AddAssign<u64>
+//{
+//}
 pub(crate) trait ProposalBounds: BorshSerialize + BorshDeserialize + Ord {}
 
-#[derive(BorshSerialize, BorshDeserialize)]
-pub struct Votes<VoterId, ProposalId, Proposal>
+#[derive(Debug, PartialEq, PartialOrd, Eq, Ord, Clone, From)]
+#[near(serializers=[borsh])]
+pub struct ProposalId(pub(crate) u64);
+
+impl AddAssign<u64> for ProposalId {
+    fn add_assign(&mut self, rhs: u64) {
+        self.0 += rhs;
+    }
+}
+
+#[near(serializers=[borsh])]
+pub struct Votes<VoterId, Proposal>
 where
     VoterId: VoterIdBounds,
-    ProposalId: ProposalIdBounds,
     Proposal: ProposalBounds,
 {
-    pub votes: IterableMap<VoterId, ProposalId>,
-    pub proposals: IterableMap<ProposalId, ProposalEntry<Proposal>>,
-    pub next_id: ProposalId,
+    pub(crate) votes: IterableMap<VoterId, ProposalId>,
+    pub(crate) proposals: IterableMap<ProposalId, ProposalEntry<Proposal>>,
+    pub(crate) next_id: ProposalId,
 }
 
 #[derive(BorshSerialize, BorshDeserialize)]
