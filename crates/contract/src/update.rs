a27f9dd740128264f1f6e653170c9f68c5019265
Patryk Bęza <patryk.beza@gmail.com>
2025-12-12 14:45:21 +0100

    fix cargo clippy

--- crates/contract/src/update.rs
+++ crates/contract/src/update.rs
@@ -12,9 +12,10 @@ use contract_interface::types::UpdateHash;
 use derive_more::Deref;
 use near_account_id::AccountId;
 use near_sdk::{
-    Gas, NearToken, Promise, env, near,
+    env, near,
     serde::{Deserialize, Serialize},
     store::IterableMap,
+    Gas, NearToken, Promise,
 };
 
 #[cfg_attr(
@@ -298,7 +299,7 @@ mod tests {
     use crate::{
         dto_mapping::IntoInterfaceType,
         primitives::test_utils::gen_account_id,
-        update::{ProposedUpdates, Update, UpdateEntry, UpdateId, bytes_used},
+        update::{bytes_used, ProposedUpdates, Update, UpdateEntry, UpdateId},
     };
     use near_account_id::AccountId;
     use near_sdk::Gas;
@@ -359,11 +360,9 @@ mod tests {
     fn test_proposed_updates_vote_update_empty() {
         let mut proposed_updates = ProposedUpdates::default();
         let account_id = gen_account_id();
-        assert!(
-            proposed_updates
-                .vote(&UpdateId(0), account_id.clone())
-                .is_none()
-        );
+        assert!(proposed_updates
+            .vote(&UpdateId(0), account_id.clone())
+            .is_none());
 
         let expected = TestUpdateVotes {
             id: 0,
@@ -528,11 +527,9 @@ mod tests {
         let found: TestUpdateVotes = (&proposed_updates).try_into().unwrap();
         assert_eq!(found, expected);
 
-        assert!(
-            proposed_updates
-                .vote(&100.into(), account_id.clone())
-                .is_none()
-        );
+        assert!(proposed_updates
+            .vote(&100.into(), account_id.clone())
+            .is_none());
 
         expected.votes = BTreeMap::new();
         let found: TestUpdateVotes = (&proposed_updates).try_into().unwrap();
