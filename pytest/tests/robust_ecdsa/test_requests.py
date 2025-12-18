7dddb9d2e718f8cf7250235a476bb59344d71349
Reynaldo Gil Pons <gilcu3@gmail.com>
2025-12-18 09:30:09 +0100

    test: add tests for robust ecdsa (#1682)

---
+++ pytest/tests/robust_ecdsa/test_requests.py
@@ -0,0 +1,14 @@
+import sys
+import pathlib
+import pytest
+
+
+sys.path.append(str(pathlib.Path(__file__).resolve().parents[1]))
+
+from common_lib import shared
+
+
+@pytest.mark.parametrize("num_requests", [3])
+@pytest.mark.no_atexit_cleanup
+def test_request_lifecycle(num_requests, shared_cluster: shared.MpcCluster):
+    shared_cluster.send_and_await_signature_requests(num_requests)
