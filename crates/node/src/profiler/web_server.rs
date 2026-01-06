665358198ff78acd047c53c806ef71bbb83f4ed0
Daniel Sharifi <me@dsharifi.com>
2026-01-06 21:44:57 +0100

    add web_server.rs

---
+++ crates/node/src/profiler/web_server.rs
@@ -0,0 +1,56 @@
+use super::pprof::collect_pprof;
+
+use axum::{
+    http::{header, StatusCode},
+    response::IntoResponse,
+};
+use std::{net::SocketAddr, time::Duration};
+use tokio::{io, net::TcpListener};
+use tower::limit::GlobalConcurrencyLimitLayer;
+
+const CONTENT_TYPE_SVG: &str = "image/svg+xml";
+const PPROF_FLAMEGRAPH_PATH: &str = "/profiler/pprof/flamegraph";
+const MAX_CONCURRENT_PPROF_REQUESTS: usize = 5;
+
+const DEFAULT_PPROF_SAMPLE_DURATION: Duration = Duration::from_secs(30);
+const DEFAULT_PPROF_SAMPLE_FREQUENCY_HZ: i32 = 1000;
+
+pub(crate) async fn start_web_server(bind_address: SocketAddr) -> Result<(), io::Error> {
+    let pprof_router = axum::Router::new()
+        .route(PPROF_FLAMEGRAPH_PATH, axum::routing::get(pprof_flamegraph))
+        .layer(GlobalConcurrencyLimitLayer::new(
+            MAX_CONCURRENT_PPROF_REQUESTS,
+        ));
+
+    let tcp_listener = TcpListener::bind(&bind_address).await?;
+
+    tokio::spawn(async move {
+        tracing::info!(?bind_address, "starting profiling server");
+        axum::serve(tcp_listener, pprof_router).await
+    });
+
+    Ok(())
+}
+
+async fn pprof_flamegraph() -> impl IntoResponse {
+    let pprof_report = collect_pprof(
+        DEFAULT_PPROF_SAMPLE_DURATION,
+        DEFAULT_PPROF_SAMPLE_FREQUENCY_HZ,
+    )
+    .await;
+
+    match pprof_report {
+        Ok(report) => {
+            let mut svg_buffer = Vec::new();
+            report.flamegraph(&mut svg_buffer).unwrap();
+
+            (
+                StatusCode::OK,
+                [(header::CONTENT_TYPE, CONTENT_TYPE_SVG)],
+                svg_buffer,
+            )
+                .into_response()
+        }
+        Err(e) => (StatusCode::INTERNAL_SERVER_ERROR, format!("Error: {:?}", e)).into_response(),
+    }
+}
