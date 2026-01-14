facf63b4376293564d47c6016902bedc548cf33b
Patryk Bęza <patryk.beza@gmail.com>
2026-01-14 12:55:17 +0100

    refactor: remove obsolete contract methods SUBMIT_PARTICIPANT_INFO and VERIFY_TEE

--- pytest/common_lib/contracts.py
+++ pytest/common_lib/contracts.py
@@ -122,6 +122,4 @@ class ContractMethod(str, Enum):
     STATE = "state"
     REGISTER_BACKUP_SERVICE = "register_backup_service"
     START_NODE_MIGRATION = "start_node_migration"
-    SUBMIT_PARTICIPANT_INFO = "submit_participant_info"
-    VERIFY_TEE = "verify_tee"
     CONFIG = "config"
