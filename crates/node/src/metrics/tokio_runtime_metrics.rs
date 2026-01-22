b5fb4b588c1dc1ed524a25aef7e5fb47f9b8763b
Daniel Sharifi <me@dsharifi.com>
2026-01-22 16:28:24 +0100

    fix typo

--- crates/node/src/metrics/tokio_runtime_metrics.rs
+++ crates/node/src/metrics/tokio_runtime_metrics.rs
@@ -5,10 +5,10 @@ use tokio_metrics::RuntimeMonitor;
 const RUNTIME_METRIC_INTERVAL: Duration = Duration::from_secs(1);
 
 // --- STABLE METRICS ---
-static TOKIO_RUNTIME_TOTAL_BUSY_DURATION_SECS: LazyLock<IntGauge> = LazyLock::new(|| {
+static TOKIO_RUNTIME_TOTAL_BUSY_DURATION_SECONDS: LazyLock<IntGauge> = LazyLock::new(|| {
     register_int_gauge!(
-        "tokio_runtime_total_busy_duration_secseconds",
-        "Total time worker threads were busy since runtime start (ms)."
+        "tokio_runtime_total_busy_duration_seconds",
+        "Total time worker threads were busy since runtime start (seconds)."
     )
     .unwrap()
 });
@@ -71,7 +71,7 @@ static TOKIO_RUNTIME_BLOCKING_QUEUE_DEPTH: LazyLock<IntGauge> = LazyLock::new(||
 
 pub(crate) async fn monitor_runtime_metrics(runtime_monitor: RuntimeMonitor) {
     for runtime_metrics in runtime_monitor.intervals() {
-        TOKIO_RUNTIME_TOTAL_BUSY_DURATION_SECS
+        TOKIO_RUNTIME_TOTAL_BUSY_DURATION_SECONDS
             .set(runtime_metrics.total_busy_duration.as_secs() as i64);
         TOKIO_RUNTIME_GLOBAL_QUEUE_DEPTH.set(runtime_metrics.global_queue_depth as i64);
         TOKIO_RUNTIME_LIVE_TASKS_COUNT.set(runtime_metrics.live_tasks_count as i64);
@@ -80,7 +80,7 @@ pub(crate) async fn monitor_runtime_metrics(runtime_monitor: RuntimeMonitor) {
         #[cfg(tokio_unstable)]
         {
             TOKIO_RUNTIME_MEAN_POLL_DURATION_SECONDS
-                .set(runtime_metrics.mean_poll_duration.as_secs() as f64);
+                .set(runtime_metrics.mean_poll_duration.as_secs_f64());
             TOKIO_RUNTIME_TOTAL_STEAL_COUNT.set(runtime_metrics.total_steal_count as i64);
             TOKIO_RUNTIME_FORCED_YIELD_COUNT.set(runtime_metrics.budget_forced_yield_count as i64);
             TOKIO_RUNTIME_BLOCKING_QUEUE_DEPTH.set(runtime_metrics.blocking_queue_depth as i64);
