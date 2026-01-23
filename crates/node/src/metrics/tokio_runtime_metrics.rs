771fcd562aaea26d0efe37da9b51af5576415d92
Daniel Sharifi <me@dsharifi.com>
2026-01-23 11:49:52 +0100

    fix: use counters instead of gauge for busy ratio

--- crates/node/src/metrics/tokio_runtime_metrics.rs
+++ crates/node/src/metrics/tokio_runtime_metrics.rs
@@ -1,13 +1,21 @@
-use prometheus::{register_gauge, register_int_gauge, Gauge, IntGauge};
+use prometheus::{register_counter, register_int_gauge, Counter, IntGauge};
 use std::sync::LazyLock;
 use tokio_metrics::RuntimeMonitor;
 
 use crate::metrics::MONITOR_SAMPLE_DURATION;
 
-static TOKIO_RUNTIME_TOTAL_BUSY_DURATION_SECONDS: LazyLock<IntGauge> = LazyLock::new(|| {
-    register_int_gauge!(
+static TOKIO_RUNTIME_TOTAL_BUSY_DURATION_SECONDS: LazyLock<Counter> = LazyLock::new(|| {
+    register_counter!(
         "tokio_runtime_total_busy_duration_seconds",
         "Total time worker threads were busy since runtime start (seconds)."
+    )
+    .unwrap()
+});
+
+static TOKIO_RUNTIME_TOTAL_TIME_ELAPSED_SECONDS: LazyLock<Counter> = LazyLock::new(|| {
+    register_counter!(
+        "tokio_runtime_total_time_elapsed_seconds",
+        "Total amount of time elapsed since observing runtime metrics (seconds)."
     )
     .unwrap()
 });
@@ -28,21 +36,14 @@ static TOKIO_RUNTIME_LIVE_TASKS_COUNT: LazyLock<IntGauge> = LazyLock::new(|| {
     .unwrap()
 });
 
-static TOKIO_RUNTIME_BUSY_RATIO: LazyLock<Gauge> = LazyLock::new(|| {
-    register_gauge!(
-        "tokio_runtime_busy_ratio",
-        "Ratio of worker busy time to elapsed time."
-    )
-    .unwrap()
-});
-
 pub(crate) async fn run_monitor_loop(runtime_monitor: RuntimeMonitor) {
     for runtime_metrics in runtime_monitor.intervals() {
         TOKIO_RUNTIME_TOTAL_BUSY_DURATION_SECONDS
-            .set(runtime_metrics.total_busy_duration.as_secs() as i64);
+            .inc_by(runtime_metrics.total_busy_duration.as_secs_f64());
+        TOKIO_RUNTIME_TOTAL_TIME_ELAPSED_SECONDS.inc_by(runtime_metrics.elapsed.as_secs_f64());
+
         TOKIO_RUNTIME_GLOBAL_QUEUE_DEPTH.set(runtime_metrics.global_queue_depth as i64);
         TOKIO_RUNTIME_LIVE_TASKS_COUNT.set(runtime_metrics.live_tasks_count as i64);
-        TOKIO_RUNTIME_BUSY_RATIO.set(runtime_metrics.busy_ratio());
 
         tokio::time::sleep(MONITOR_SAMPLE_DURATION).await;
     }
