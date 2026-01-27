d82bcf04a395b19445ee33286cb17f8549755786
Barakeinav1 <barakeinav@gmail.com>
2026-01-27 14:06:26 +0000

    update scriptes

---
+++ deployment/testnet/scripts/check_pre_signatures_generated_v2.sh
@@ -0,0 +1,15 @@
+for i in {1..5}; do
+  echo -n "node$i: "
+  curl -s http://51.68.219.$i:18082/debug/tasks | awk '/generate presignatures for domain 0/ {
+    match($0, /available: ([0-9]+)\/([0-9]+)/, a)
+    print a[1] "/" a[2]
+  }'
+done
+
+for i in {6..10}; do
+  echo -n "node$i: "
+  curl -s http://5.196.36.$((113+i-6)):18082/debug/tasks | awk '/generate presignatures for domain 0/ {
+    match($0, /available: ([0-9]+)\/([0-9]+)/, a)
+    print a[1] "/" a[2]
+  }'
+done
