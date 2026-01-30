ab8c039a7f840d41efb2673dc48bbcf86d1a680a
Daniel Sharifi <me@dsharifi.com>
2026-01-31 00:19:02 +0100

    add signature types to primitives

--- crates/contract-interface/src/types/primitives.rs
+++ crates/contract-interface/src/types/primitives.rs
@@ -1,5 +1,6 @@
 use borsh::{BorshDeserialize, BorshSerialize};
 use serde::{Deserialize, Serialize};
+use serde_with::serde_as;
 
 /// A Near AccountId
 #[derive(
@@ -87,3 +88,61 @@ pub struct Tweak(pub [u8; 32]);
     derive(schemars::JsonSchema, borsh::BorshSchema)
 )]
 pub struct DomainId(pub u64);
+
+#[derive(Debug, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Serialize, Deserialize)]
+#[cfg_attr(
+    all(feature = "abi", not(target_arch = "wasm32")),
+    derive(schemars::JsonSchema)
+)]
+#[serde(tag = "scheme")]
+pub enum SignatureResponse {
+    Secp256k1(K256Signature),
+    Ed25519 { signature: Ed25519Signature },
+}
+
+#[serde_as]
+#[derive(Debug, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Serialize, Deserialize)]
+#[cfg_attr(
+    all(feature = "abi", not(target_arch = "wasm32")),
+    derive(schemars::JsonSchema)
+)]
+pub struct Ed25519Signature(
+    #[cfg_attr(
+            all(feature = "abi", not(target_arch = "wasm32")),
+            schemars(with = "Vec<u8>") // Schemars doesn't support arrays of size greater than 32. 
+        )]
+    #[serde_as(as = "[_; 64]")]
+    [u8; 64],
+);
+
+#[serde_as]
+#[derive(Debug, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Serialize, Deserialize)]
+#[cfg_attr(
+    all(feature = "abi", not(target_arch = "wasm32")),
+    derive(schemars::JsonSchema)
+)]
+pub struct K256Signature {
+    pub big_r: AffinePoint,
+    pub s: K256Scalar,
+    pub recovery_id: u8,
+}
+
+#[serde_as]
+#[derive(Debug, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Serialize, Deserialize)]
+#[cfg_attr(
+    all(feature = "abi", not(target_arch = "wasm32")),
+    derive(schemars::JsonSchema)
+)]
+pub struct AffinePoint {
+    // TODO: Figure out how to represent affine_point to be backwards compatible with
+    // SerializableAffinePoint in contract::crypto_shared::SerializableAffinePoint
+    pub affine_point: Vec<u8>,
+}
+
+#[serde_as]
+#[derive(Debug, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Serialize, Deserialize)]
+#[cfg_attr(
+    all(feature = "abi", not(target_arch = "wasm32")),
+    derive(schemars::JsonSchema)
+)]
+pub struct K256Scalar(pub [u8; 32]);
