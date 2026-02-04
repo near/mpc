bc86f330b7e212bdb5223fea46a57e794a52194c
kevindeforth <32777623+kevindeforth@users.noreply.github.com>
2026-02-04 10:08:46 +0100

    fix: properly fix network race condition (#1831)
    Co-authored-by: Mårten Blankfors <marten@blankfors.se>

--- crates/node/src/protocol_version.rs
+++ crates/node/src/protocol_version.rs
@@ -1,17 +1,18 @@
-/// This must be incremented every time we introduce an incompatible protocol
+use derive_more::Display;
+use num_enum::{FromPrimitive, IntoPrimitive};
+
+/// the current protocol version
+pub const CURRENT_PROTOCOL_VERSION: CommunicationProtocols = CommunicationProtocols::Jan2026;
+
+/// This must be extended every time we introduce an incompatible protocol
 /// change.
-///
-/// A change is an incompatible protocol change UNLESS a binary compiled with
-/// the code before the change is compatible with a binary compiled after the
-/// change.
-///
-/// A binary A is compatible with binary B if a cluster of nodes where some are
-/// running A and some are running B can still function normally, even if
-/// neither group have threshold number of nodes.
-///
-/// The effect of this protocol version is that nodes with different protocol
-/// versions will refuse to connect to each other. That way, when we introduce
-/// an incompatible protocol change, we are effectively creating a new network
-/// that is separate from the old network, thus requiring only threshold number
-/// of nodes to upgrade.
-pub const MPC_PROTOCOL_VERSION: u32 = 7;
+#[derive(Debug, Copy, Clone, IntoPrimitive, FromPrimitive, PartialEq, Display)]
+#[repr(u32)]
+pub enum CommunicationProtocols {
+    #[num_enum(alternatives = [1..7])]
+    Unsupported = 0,
+    Dec2025 = 7,
+    Jan2026 = 8,
+    #[num_enum(catch_all)]
+    Unknown(u32),
+}
