f7cde70682eb3afa3397135550cc908367feeb20
Nodar <nodar.ambroladze@gmail.com>
2026-03-03 12:14:26 +0100

    Don't panic

--- crates/threshold-signatures/src/thresholds.rs
+++ crates/threshold-signatures/src/thresholds.rs
@@ -27,6 +27,6 @@ impl ReconstructionLowerBound {
 /// Lower bound to reconstruct the secret is MaxMalicious + 1.
 impl From<MaxMalicious> for ReconstructionLowerBound {
     fn from(m: MaxMalicious) -> Self {
-        Self(m.0.checked_add(1).expect("MaxMalicious too large"))
+        Self(m.0.saturating_add(1))
     }
 }
