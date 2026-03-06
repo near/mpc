6f90606a32a01e045127578371dac62c67f7ef2e
Daniel Sharifi <me@dsharifi.com>
2026-03-06 16:54:30 +0100

    fmt

--- crates/node/src/config/start.rs
+++ crates/node/src/config/start.rs
@@ -40,7 +40,13 @@ impl std::fmt::Debug for SecretsStartConfig {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.debug_struct("SecretsStartConfig")
             .field("secret_store_key_hex", &"[REDACTED]")
-            .field("backup_encryption_key_hex", &self.backup_encryption_key_hex.as_ref().map(|_| "[REDACTED]"))
+            .field(
+                "backup_encryption_key_hex",
+                &self
+                    .backup_encryption_key_hex
+                    .as_ref()
+                    .map(|_| "[REDACTED]"),
+            )
             .finish()
     }
 }
