27df798dea9ca4aec4c5afdd8b8db911b84bbce9
SimonRastikian <43679791+SimonRastikian@users.noreply.github.com>
2026-03-13 10:05:08 +0100

    Adding pytests

--- pytest/tests/robust_ecdsa/__init__.py
+++ pytest/tests/robust_ecdsa/__init__.py
@@ -1,1 +1,8 @@
 # TODO(#1690): tests in this module are almost the same as the shared_cluster_tests. Eventually they should be unified
+import pytest
+
+# V2Secp256k1 has been removed from the DTO SignatureScheme enum.
+# These tests need to be re-enabled once the DamgardEtAl protocol is surfaced via the KeyConfig DTO.
+pytestmark = pytest.mark.skip(
+    reason="V2Secp256k1 removed from DTO; re-enable when DamgardEtAl protocol is surfaced via KeyConfig DTO"
+)
