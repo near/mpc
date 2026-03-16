35dc20b96aa0d6f2f9aadb138fafd32a2e2b68b6
Daniel Sharifi <40335219+DSharifi@users.noreply.github.com>
2026-03-16 16:16:16 +0100

    feat(node): start with config file option also initializes neard (#2455)

--- crates/node/src/config.rs
+++ crates/node/src/config.rs
@@ -15,7 +15,8 @@ use std::{
 
 mod start;
 pub use start::{
-    GcpStartConfig, SecretsStartConfig, StartConfig, TeeAuthorityStartConfig, TeeStartConfig,
+    ChainId, DownloadConfigType, GcpStartConfig, NearInitConfig, SecretsStartConfig, StartConfig,
+    TeeAuthorityStartConfig, TeeStartConfig,
 };
 
 mod foreign_chains;
