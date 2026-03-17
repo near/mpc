b036832ef6ea3a0102c7f6f06cb9d05984e6b5c3
Kevin Deforth <32777623+kevindeforth@users.noreply.github.com.>
2026-03-17 10:32:18 +0400

    wrap rpc handler in arc

--- crates/chain-gateway/src/near_internals_wrapper/rpc.rs
+++ crates/chain-gateway/src/near_internals_wrapper/rpc.rs
@@ -1,16 +1,20 @@
+use std::sync::Arc;
+
 use crate::{errors::NearRpcError, primitives::SubmitSignedTransaction};
 
 /// Arc-Wrapper around near-internal handler struct
 #[derive(Clone)]
 pub(crate) struct NearRpcActorHandle {
-    rpc_handler: near_async::multithread::MultithreadRuntimeHandle<near_client::RpcHandler>,
+    rpc_handler: Arc<near_async::multithread::MultithreadRuntimeHandle<near_client::RpcHandler>>,
 }
 
 impl NearRpcActorHandle {
     pub(crate) fn new(
         rpc_handler: near_async::multithread::MultithreadRuntimeHandle<near_client::RpcHandler>,
     ) -> Self {
-        Self { rpc_handler }
+        Self {
+            rpc_handler: Arc::new(rpc_handler),
+        }
     }
 }
 
@@ -22,7 +26,7 @@ impl SubmitSignedTransaction for NearRpcActorHandle {
         transaction: near_indexer::near_primitives::transaction::SignedTransaction,
     ) -> Result<(), Self::Error> {
         let response = near_async::messaging::CanSendAsync::send_async(
-            &self.rpc_handler,
+            self.rpc_handler.as_ref(),
             near_client::ProcessTxRequest {
                 transaction,
                 is_forwarded: false,
