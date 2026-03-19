537255b9b64bef031b6161d53a61282af5ee5f83
Patrick <patryk.beza@gmail.com>
2026-03-19 18:03:10 +0100

    feat: add `tee-context` crate (#2505)

--- crates/chain-gateway/src/transaction_sender/traits.rs
+++ crates/chain-gateway/src/transaction_sender/traits.rs
@@ -2,6 +2,7 @@ use near_account_id::AccountId;
 use near_indexer_primitives::CryptoHash;
 use near_indexer_primitives::types::Gas;
 
+use crate::ChainGateway;
 use crate::errors::{ChainGatewayError, ChainGatewayOp};
 use crate::primitives::{FetchLatestFinalBlockInfo, SubmitSignedTransaction};
 use crate::transaction_sender::TransactionSigner;
@@ -68,6 +69,44 @@ where
                 message: err.to_string(),
             })?;
         Ok(tx_hash)
+    }
+}
+
+pub trait SubmitTransaction: Send + Sync + 'static {
+    type Error: std::error::Error + Send + Sync + 'static;
+    fn submit(
+        &self,
+        receiver_id: AccountId,
+        method_name: &str,
+        args: Vec<u8>,
+    ) -> impl Future<Output = Result<(), Self::Error>> + Send;
+}
+
+#[derive(derive_more::Constructor)]
+pub struct TransactionSender {
+    chain_gateway: ChainGateway,
+    signer: TransactionSigner,
+}
+
+impl SubmitTransaction for TransactionSender {
+    type Error = ChainGatewayError;
+    async fn submit(
+        &self,
+        receiver_id: AccountId,
+        method_name: &str,
+        args: Vec<u8>,
+    ) -> Result<(), ChainGatewayError> {
+        const MAX_GAS: Gas = Gas::from_teragas(300);
+        self.chain_gateway
+            .submit_function_call_tx(
+                &self.signer,
+                receiver_id,
+                method_name.to_string(),
+                args,
+                MAX_GAS,
+            )
+            .await?;
+        Ok(())
     }
 }
 
