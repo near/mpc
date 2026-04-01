461a97fb6c2a24720d01286fb776f76e5628f5f2
Nodar <nodar.ambroladze@gmail.com>
2026-04-01 16:34:13 +0200

    test: Implement NearSandbox, NearBlockchain and request lifecycle test (#2590)

--- crates/e2e-tests/src/lib.rs
+++ crates/e2e-tests/src/lib.rs
@@ -4,7 +4,10 @@ pub mod mpc_node;
 pub mod near_sandbox;
 pub mod port_allocator;
 
-pub use blockchain::{DeployedContract, NearBlockchain};
-pub use cluster::{MpcCluster, MpcClusterConfig, MpcNodeState};
+pub use blockchain::{ClientHandle, DeployedContract, NearBlockchain};
+pub use cluster::{
+    DEFAULT_PRESIGNATURES_TO_BUFFER, DEFAULT_TRIPLES_TO_BUFFER, MpcCluster, MpcClusterConfig,
+    MpcNodeState,
+};
 pub use near_sandbox::NearSandbox;
 pub use port_allocator::E2ePortAllocator;
