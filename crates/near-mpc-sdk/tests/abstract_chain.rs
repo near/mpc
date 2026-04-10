f5548caafa522bc3b02daa4a0acc860fb84f166a
Daniel Sharifi <me@dsharifi.com>
2026-04-10 16:38:20 +0200

    add sanity check integration tests

---
+++ crates/near-mpc-sdk/tests/abstract_chain.rs
@@ -0,0 +1,26 @@
+use assert_matches::assert_matches;
+use near_mpc_sdk::foreign_chain::{
+    DomainId, ForeignChainRequestBuilder,
+    abstract_chain::{EvmFinality, EvmTxId, ForeignChainRpcRequest},
+};
+
+#[test]
+fn no_extractor_added() {
+    // given
+    let domain_id = DomainId::from(2);
+    let tx_id = EvmTxId::from([123; 32]);
+
+    // when
+    let (_verifier, built_sign_request_args) = ForeignChainRequestBuilder::new_abstract()
+        .with_tx_id(tx_id)
+        .with_finality(EvmFinality::Finalized)
+        .with_domain_id(domain_id)
+        .build();
+
+    // then
+    let no_extractors = vec![];
+
+    assert_matches!(built_sign_request_args.request, ForeignChainRpcRequest::Abstract(abstract_rpc_request) => {
+        assert_eq!(abstract_rpc_request.extractors, no_extractors);
+    });
+}
