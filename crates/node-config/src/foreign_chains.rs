fcddea5fbc5db4822f7cf6ea6aa0917238b996ae
Daniel Sharifi <me@dsharifi.com>
2026-04-23 00:38:54 +0200

    add comment to force CI retrigger

--- crates/node-config/src/foreign_chains.rs
+++ crates/node-config/src/foreign_chains.rs
@@ -15,6 +15,7 @@ mod auth;
 
 #[derive(Clone, Debug, Default, Serialize, Deserialize, PartialEq, Eq)]
 #[serde(deny_unknown_fields)]
+// TODO(#3002): only keep variants that are actually supported by the node binary
 pub struct ForeignChainsConfig {
     #[serde(default, skip_serializing_if = "Option::is_none")]
     pub solana: Option<ForeignChainConfig>,
