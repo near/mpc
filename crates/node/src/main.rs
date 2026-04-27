fef46cfaacce3fde27c0a437779989d8ab6f0a07
Daniel Sharifi <me@dsharifi.com>
2026-04-27 21:09:43 +0200

    Revert "chore: remove the use of jemalloc (#1935)"
    This reverts commit fdb528f7fc4c151fae73b8a6f291ee166d2d9491.

--- crates/node/src/main.rs
+++ crates/node/src/main.rs
@@ -1,5 +1,9 @@
 use clap::Parser;
 use mpc_node::cli;
+use tikv_jemallocator::Jemalloc;
+
+#[global_allocator]
+static GLOBAL_ALLOCATOR: Jemalloc = Jemalloc;
 
 fn main() -> anyhow::Result<()> {
     // Install the default rustls crypto provider before any TLS usage.
