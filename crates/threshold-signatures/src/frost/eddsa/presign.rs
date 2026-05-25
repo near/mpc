d388175f69c6d2fd53cb2f343ff82d3c6fde3b10
Simon Rastikian <43679791+SimonRastikian@users.noreply.github.com>
2026-05-25 15:13:13 +0200

    Reorganizing the interface for frost

---
+++ crates/threshold-signatures/src/frost/eddsa/presign.rs
@@ -0,0 +1,35 @@
+use crate::{
+    crypto::ciphersuite::{BytesOrder, ScalarSerializationFormat},
+    errors::InitializationError,
+    participants::Participant,
+    protocol::Protocol,
+    Ciphersuite,
+};
+use rand_core::CryptoRngCore;
+
+pub use frost_ed25519::Ed25519Sha512;
+
+impl ScalarSerializationFormat for Ed25519Sha512 {
+    fn bytes_order() -> BytesOrder {
+        BytesOrder::LittleEndian
+    }
+}
+
+impl Ciphersuite for Ed25519Sha512 {}
+
+/// Signature would be Some for coordinator and None for other participants
+pub type SignatureOption = Option<frost_ed25519::Signature>;
+
+pub type KeygenOutput = crate::KeygenOutput<Ed25519Sha512>;
+pub type PresignArguments = crate::frost::PresignArguments<Ed25519Sha512>;
+pub type PresignOutput = crate::frost::PresignOutput<Ed25519Sha512>;
+
+/// Ed25519 presigning function
+pub fn presign(
+    participants: &[Participant],
+    me: Participant,
+    args: &PresignArguments,
+    rng: impl CryptoRngCore + Send + 'static,
+) -> Result<impl Protocol<Output = PresignOutput>, InitializationError> {
+    crate::frost::presign(participants, me, args, rng)
+}
---
+++ crates/threshold-signatures/src/frost/redjubjub/presign.rs
@@ -0,0 +1,36 @@
+use crate::{
+    crypto::ciphersuite::{BytesOrder, ScalarSerializationFormat},
+    errors::InitializationError,
+    participants::Participant,
+    protocol::Protocol,
+    Ciphersuite,
+};
+use rand_core::CryptoRngCore;
+use reddsa::frost::redjubjub::Signature;
+
+// JubJub + Blake2b512 Ciphersuite
+pub use reddsa::frost::redjubjub::JubjubBlake2b512;
+
+impl ScalarSerializationFormat for JubjubBlake2b512 {
+    fn bytes_order() -> BytesOrder {
+        BytesOrder::LittleEndian
+    }
+}
+impl Ciphersuite for JubjubBlake2b512 {}
+
+pub type KeygenOutput = crate::KeygenOutput<JubjubBlake2b512>;
+pub type PresignArguments = crate::frost::PresignArguments<JubjubBlake2b512>;
+pub type PresignOutput = crate::frost::PresignOutput<JubjubBlake2b512>;
+
+/// Signature would be Some for coordinator and None for other participants
+pub type SignatureOption = Option<Signature>;
+
+/// `RedJubJub` presigning function
+pub fn presign(
+    participants: &[Participant],
+    me: Participant,
+    args: &PresignArguments,
+    rng: impl CryptoRngCore + Send + 'static,
+) -> Result<impl Protocol<Output = PresignOutput>, InitializationError> {
+    crate::frost::presign(participants, me, args, rng)
+}
