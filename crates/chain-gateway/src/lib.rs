ac826f85750a9e79320f758551fc339b0b888c64
Haiyue Chen <haiyue.chen@nearone.org>
2026-08-03 14:25:25 +0200

    chore: group the cfg gated module with the other public ones

--- crates/chain-gateway/src/lib.rs
+++ crates/chain-gateway/src/lib.rs
@@ -1,6 +1,8 @@
 pub mod chain_gateway;
 pub mod errors;
 pub mod event_subscriber;
+#[cfg(any(test, feature = "test-utils"))]
+pub mod mock;
 pub mod primitives;
 pub mod state_viewer;
 pub mod transaction_sender;
@@ -9,6 +11,3 @@ pub mod types;
 mod near_internals_wrapper;
 
 pub use chain_gateway::{ChainGateway, NodeHandle};
-
-#[cfg(any(test, feature = "test-utils"))]
-pub mod mock;
